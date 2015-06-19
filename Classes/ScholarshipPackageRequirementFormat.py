78532bb1a75a8a464c89e6264562df55ca56bb0e
dmerson <dmerson@email.arizona.edu>
2015-06-19 12:21:15 -0700

    Added extra fields for class.

--- Classes/ScholarshipPackageRequirementFormat.py
+++ Classes/ScholarshipPackageRequirementFormat.py
@@ -1,5 +1,6 @@
 class ScholarshipPackageRequirement(object):
-    def __init__(self,scholarshipPackageId,requirementType,requirementValue):
+    def __init__(self,scholarshipPackageId,requirementType,requirementValue,logicGroup):
+        self.logicGroup = logicGroup
         self.requirementValue = requirementValue
         self.requirementType = requirementType
         self.scholarshipPackageId = scholarshipPackageId
