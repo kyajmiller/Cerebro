cb9972f4b25082652de1dd749c4b6ed15357bc31
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-19 12:17:13 -0700

    unit tests for the getScholarshipPackageRequirementFunction

--- TestGPA.py
+++ TestGPA.py
@@ -5,14 +5,21 @@ class TestStringMethods(unittest.TestCase):
     def test_GPA(self):
         testgpa = GPA('The GPA is 3.159 not 5.304')
         self.assertIsNotNone(testgpa)
-        self.assertEqual(testgpa.checkContext('gpa'), True)
-        self.assertEqual(testgpa.getGPA(), ['3.159'])
+        self.assertEqual(testgpa.checkContext('gpa|grade\spoint\saverage|maintain'), True)
+        self.assertEqual(testgpa.getGPA(), '3.159')
 
         failgpa = GPA("This is not the GPA you're looking for 7.4")
         self.assertIsNotNone(failgpa)
-        self.assertEqual(failgpa.checkContext('gpa'), True)
-        self.assertNotEqual(failgpa.getGPA(), ['7.4'])
-        self.assertEqual(failgpa.getGPA(), [])
+        self.assertEqual(failgpa.checkContext('gpa|grade\spoint\saverage|maintain'), True)
+        self.assertNotEqual(failgpa.getGPA(), '7.4')
+        self.assertEqual(failgpa.getGPA(), '')
+
+        testSPRFforGPA = GPA('This is to test the SPRF function for the grade point average of 2.9')
+        self.assertIsNotNone(testSPRFforGPA)
+        self.assertEqual(testSPRFforGPA.checkContext('gpa|grade\spoint\saverage|maintain'), True)
+        self.assertEqual(testSPRFforGPA.getGPA(), '2.9')
+        self.assertEqual(testSPRFforGPA.getScholarshipPackageRequirementFormat(), 'AttributeId = 1, RequirementTypeCode = >=, RequirementValue = 2.9, LogicGroup = 0')
+
 
 
 if __name__ == '__main__':
