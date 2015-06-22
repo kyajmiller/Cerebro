4ed30cc821e7b8ebfb079510b7098c33bb12568b
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-22 10:05:35 -0700

    each unit test now has own function

--- TestGPA.py
+++ TestGPA.py
@@ -9,17 +9,21 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(testgpa.checkContext('gpa|grade\spoint\saverage|maintain'), True)
         self.assertEqual(testgpa.getGPA(), '3.159')
 
+    def test_GPAfailure(self):
         failgpa = GPA("This is not the GPA you're looking for 7.4")
         self.assertIsNotNone(failgpa)
         self.assertEqual(failgpa.checkContext('gpa|grade\spoint\saverage|maintain'), True)
         self.assertNotEqual(failgpa.getGPA(), '7.4')
         self.assertEqual(failgpa.getGPA(), '')
 
+    def test_GPAvsDollarSign(self):
         testGPANoDollarSign = GPA('$3.50 is money, but 3.49 is a GPA')
         self.assertIsNotNone(testGPANoDollarSign)
         self.assertEqual(testGPANoDollarSign.checkContext('gpa|grade\spoint\saverage|maintain'), True)
         self.assertNotEqual(testGPANoDollarSign.getGPA(), '3.50')
         self.assertEqual(testGPANoDollarSign.getGPA(), '3.49')
+
+    def test_GPACreateSPRFClass(self):
         testSPRFforGPA = GPA('This is to test the SPRF function for the grade point average of 2.9')
         self.assertIsNotNone(testSPRFforGPA)
         self.assertEqual(testSPRFforGPA.checkContext('gpa|grade\spoint\saverage|maintain'), True)
