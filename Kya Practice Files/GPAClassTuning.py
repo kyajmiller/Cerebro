ba0a9551f90fb961cf396ee8d4681e6ca921ef8f
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-22 12:05:37 -0700

    more testing on the test cases, cleaned up the text in order for the regex to work better

--- Kya Practice Files/GPAClassTuning.py
+++ Kya Practice Files/GPAClassTuning.py
@@ -1,5 +1,6 @@
 from Classes.GPA import GPA
 import pyodbc
+import re
 
 def ConnectToTheDatabase():
     global cnxn, cursor
@@ -14,14 +15,40 @@ ConnectToTheDatabase()
 GetInfoFromDatabase()
 
 Eligibilities = []
+ScholarshipIds = []
 while 1:
     row = cursor.fetchone()
     if not row:
         break
     Eligibilities.append(row.Elgibility)
+    ScholarshipIds.append(row.ScholarshipPackageId)
 
-for Eligibility in Eligibilities:
-    testGPA = GPA(Eligibility)
+for e in range(len(Eligibilities)):
+    Eligibility = Eligibilities[e]
+    print(ScholarshipIds[e])
+
+    FormattedEligibilities = []
+    StripULTag = re.sub('<ul>|</ul>', '', Eligibility)
+    Eligibility = StripULTag
+    IndividualEligibilities = Eligibility.split('</li>')
+    for IndividualEligibility in IndividualEligibilities:
+        IndividualEligibility.strip('\r')
+        IndividualEligibility.strip('\n')
+        StripXMLTags = re.sub('<\/*[a-z]*\s*\/*>', '', IndividualEligibility)
+        #need to remove the xml tags first (specificially the end of line) or else the next one will capture the whole
+        #string and delete it
+        StripRemainingTags = re.sub('<.*>', '', StripXMLTags)
+        IndividualEligibility = StripRemainingTags
+        FormattedEligibilities.append(IndividualEligibility)
+
+    JoinEligibilitytoString = ', '.join(FormattedEligibilities)
+    StripApostrophe = re.sub('\'*\"*', '', JoinEligibilitytoString)
+    Stripnbsp = re.sub('&nbsp;', '', StripApostrophe)
+    JoinEligibilitytoString = Stripnbsp
+
+    Eligibility = JoinEligibilitytoString
+
+    testGPA = GPA(Eligibility, ScholarshipIds[e])
     print(Eligibility)
     print(testGPA.getGPA())
     print('\n\n')
