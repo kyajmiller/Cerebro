5648a6d837f739513afa847105426fc73a9f0561
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-22 13:59:54 -0700

    getMajors now returns string value instead of list, returns self.requirementValue instead of self.resultList

--- Classes/Majors.py
+++ Classes/Majors.py
@@ -2,11 +2,16 @@ from Classes.Parser import Parser
 
 
 class Majors(Parser):
-    def __init__(self, stringToScan, majorOptions):
+    def __init__(self, stringToScan, scholarshipPackageId, majorOptions):
         self.majorOptions = majorOptions
         self.stringToScan = stringToScan
         Parser.__init__(self, self.stringToScan.lower(), self.majorOptions)
         self.resultList = []
+        self.attributeId = '1'
+        self.requirementValue = ''
+        self.logicGroup = '0'
+        self.requirementTypeCode = '>='
+        self.scholarshipPackageId = scholarshipPackageId
 
     def checkContext(self, contextCriteria):
         contextChecker = Parser(self.stringToScan.lower(), contextCriteria)
@@ -19,4 +24,6 @@ class Majors(Parser):
                 self.resultList.append(i)
 
         self.resultList = list(set(self.resultList))
-        return self.resultList
+
+        self.requirementValue = ', '.join(self.resultList)
+        return self.requirementValue
