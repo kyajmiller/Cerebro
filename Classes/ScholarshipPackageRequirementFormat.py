880e187a2e322d1305e2ed5ca94be719fa29d6c1
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-22 10:07:32 -0700

    added attributeId argument and value

--- Classes/ScholarshipPackageRequirementFormat.py
+++ Classes/ScholarshipPackageRequirementFormat.py
@@ -1,5 +1,6 @@
 class ScholarshipPackageRequirement(object):
-    def __init__(self,scholarshipPackageId,requirementType,requirementValue,logicGroup):
+    def __init__(self, scholarshipPackageId, attributeId, requirementType, requirementValue, logicGroup):
+        self.attributeId = attributeId
         self.logicGroup = logicGroup
         self.requirementValue = requirementValue
         self.requirementType = requirementType
