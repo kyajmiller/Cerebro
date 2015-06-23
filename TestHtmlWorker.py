bac2e09c939f4e034bae48dbb6364f68c648c2c5
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-23 10:47:07 -0700

    reformat

--- TestHtmlWorker.py
+++ TestHtmlWorker.py
@@ -10,14 +10,15 @@ class TestHtmlWorker(unittest.TestCase):
       result=HTMLWorker.removeAllJavascript("<javascript>item</javascript>")
       self.assertEqual("item",result)
 
-
   def testTagRemover(self):
       result=HTMLWorker.removeHtmlTags("<body><a href='test'>item</a><div>This is the end</div></body>")
       self.assertEqual("  item  This is the end  ",result)
+
   def testRemoveNonBodyObjects(self):
       result=HTMLWorker.removeNonBodyElements("<html><head>test</head><body><a href='test'>item</a><div>This is the end</div></body>")
       result=HTMLWorker.removeHtmlTags(result)
       self.assertEqual(" item  This is the end ",result)
+
   def testPreProcessing(self):
       htmlpage='''<html><script>var x=1;</script><head>Test</head><body><div id='test'><ol><li>test</li></ol></div></body></html>'''
       result=HTMLWorker.cleanWebPageBeforeProcessing(htmlpage)
