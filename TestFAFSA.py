abdadd890bfd6793616ef214e8d09c9bcd46d28e
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-23 10:45:04 -0700

    reformat

--- TestFAFSA.py
+++ TestFAFSA.py
@@ -7,6 +7,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNotNone(testfafsa)
         self.assertEqual(testfafsa.getFAFSA(), ['yes'])
 
+    def test_FAFSAFailure(self):
         failfafsa = FAFSA('This string does not talk about financial aid.')
         self.assertIsNotNone(failfafsa)
         self.assertNotEqual(failfafsa.getFAFSA(), ['yes'])
