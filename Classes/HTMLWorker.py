2e379018f3c8e6491b41030a51e27e52f23211ba
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-23 14:03:24 -0700

    reformat

--- Classes/HTMLWorker.py
+++ Classes/HTMLWorker.py
@@ -1,22 +1,24 @@
 import re
-class HTMLWorker(object):
 
-  @staticmethod
-  def removeHtmlTags(stringToParse):
+
+class HTMLWorker(object):
+    @staticmethod
+    def removeHtmlTags(stringToParse):
         return re.sub('''<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^\'">\s]+))?)+\s*|\s*)/?>''', ' ', stringToParse)
 
-  @staticmethod
-  def removeAllJavascript(stringToParse):
-      return re.sub('''<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^\'">\s]+))?)+\s*|\s*)/?>''', '', stringToParse)
+    @staticmethod
+    def removeAllJavascript(stringToParse):
+        return re.sub('''<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^\'">\s]+))?)+\s*|\s*)/?>''', '', stringToParse)
 
-  @staticmethod
-  def removeNonBodyElements(stringToParse):
-      result= re.sub('''<html>[\s\S]*<body>''', '', stringToParse)
-      result= re.sub('''</body>[\s\S]*</html>''', '', result)
-      return re.sub('''</body>''', '', result)
-  @staticmethod
-  def cleanWebPageBeforeProcessing(stringToParse):
-      result=HTMLWorker.removeNonBodyElements(stringToParse)
-      result=HTMLWorker.removeAllJavascript(result)
-      result=HTMLWorker.removeHtmlTags(result)
-      return result
+    @staticmethod
+    def removeNonBodyElements(stringToParse):
+        result = re.sub('''<html>[\s\S]*<body>''', '', stringToParse)
+        result = re.sub('''</body>[\s\S]*</html>''', '', result)
+        return re.sub('''</body>''', '', result)
+
+    @staticmethod
+    def cleanWebPageBeforeProcessing(stringToParse):
+        result = HTMLWorker.removeNonBodyElements(stringToParse)
+        result = HTMLWorker.removeAllJavascript(result)
+        result = HTMLWorker.removeHtmlTags(result)
+        return result
