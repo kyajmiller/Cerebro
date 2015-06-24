4942571a3cad147cfbf0bb7fb4a22540185c13db
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-24 11:03:18 -0700

    added getScholarshipPackageRequirementFormat function

--- Classes/UACollege.py
+++ Classes/UACollege.py
@@ -1,11 +1,18 @@
 from Classes.Parser import Parser
+from Classes.ScholarshipPackageRequirementFormat import ScholarshipPackageRequirement
+
 
 class UACollege(Parser):
-    def __init__(self, stringToScan, uaColleges):
+    def __init__(self, stringToScan, scholarshipPackageId, uaColleges):
         self.uaColleges = uaColleges
         self.stringToScan = stringToScan
         Parser.__init__(self, self.stringToScan.lower(), self.uaColleges)
         self.resultList = []
+        self.attributeId = '377'
+        self.requirementValue = ''
+        self.logicGroup = '0'
+        self.requirementTypeCode = '*'
+        self.scholarshipPackageId = scholarshipPackageId
 
     def checkContext(self, contextCriteria):
         contextChecker = Parser(self.stringToScan.lower(), contextCriteria)
@@ -17,4 +24,14 @@ class UACollege(Parser):
                 self.resultList.append(i)
 
         self.resultList = list(set(self.resultList))
-        return self.resultList
+
+        self.requirementValue = ', '.join(self.resultList)
+        return self.requirementValue
+
+    def getScholarshipPackageRequirementFormat(self):
+        if self.getUACollege() != '':
+            UACollege_SPRF = ScholarshipPackageRequirement(self.scholarshipPackageId, self.attributeId,
+                                                           self.requirementTypeCode, self.getUACollege(),
+                                                           self.logicGroup)
+
+            return UACollege_SPRF
