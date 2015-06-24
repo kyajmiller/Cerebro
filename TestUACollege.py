34d200409607ec9685011fdbc0a2248e1c505e39
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-24 12:22:21 -0700

    updated expected outputs to reflect removal of ignore case

--- TestUACollege.py
+++ TestUACollege.py
@@ -1,13 +1,13 @@
 import unittest
 from Classes.UACollege import UACollege
 
-UAColleges = ['agriculture & life sciences', 'agriculture and life sciences',
-              'architecture, planning & landscape architecture',
-              'architecture planning and landscape architecture', 'education', 'engineering', 'fine arts', 'humanities',
-              'medicine', 'nursing', 'optical science', 'pharmacy', 'science', 'social and behavioral science',
-              'letters, arts and science',
-              'management', 'eller', 'honors', 'law', 'james e. rogers', 'public health', 'outreach college',
-              'graduate college', 'school of art', 'landscape architecture', 'snre']
+UAColleges = ['Agriculture & Life Sciences', 'Agriculture and Life Sciences',
+              'Architecture, Planning & Landscape Architecture',
+              'Architecture Planning and Landscape Architecture', 'Education', 'Engineering', 'Fine Arts', 'Humanities',
+              'Medicine', 'Nursing', 'Optical Science', 'Pharmacy', 'Science', 'Social and Behavioral Sciences',
+              'Letters, Arts and Science',
+              'Management', 'Eller', 'Honors', 'Law', 'James E. Rogers', 'Public Health', 'Outreach College',
+              'Graduate College', 'School of Art', 'Landscape Architecture', 'SNRE', 'COM']
 
 UAColleges = '|'.join(UAColleges)
 
@@ -17,7 +17,7 @@ class TestStringMethods(unittest.TestCase):
         testuacollege = UACollege('I am in the College of Social and Behavioral Sciences.', '2', UAColleges)
         self.assertIsNotNone(testuacollege)
         self.assertEqual(True, testuacollege.checkContext('college|college\sof|school\sof'))
-        self.assertEqual('social and behavioral science', testuacollege.getUACollege())
+        self.assertEqual('Social and Behavioral Sciences', testuacollege.getUACollege())
 
     def test_UACollegeFailure(self):
         failuacollege = UACollege('Majoring in optical science', '3', UAColleges)
@@ -31,13 +31,13 @@ class TestStringMethods(unittest.TestCase):
             'This is to test the SPRF function for the UA College of Agriculture and Life Sciences', '1543', UAColleges)
         self.assertIsNotNone(testSPRFforUACollege)
         self.assertEqual(True, testSPRFforUACollege.checkContext('college|college\sof|school\sof'))
-        self.assertEqual('agriculture and life sciences', testSPRFforUACollege.getUACollege())
+        self.assertEqual('Agriculture and Life Sciences', testSPRFforUACollege.getUACollege())
         self.assertIsNotNone(testSPRFforUACollege.getScholarshipPackageRequirementFormat())
         test_SPRF = testSPRFforUACollege.getScholarshipPackageRequirementFormat()
         self.assertEqual('1543', test_SPRF.scholarshipPackageId)
         self.assertEqual('377', test_SPRF.attributeId)
         self.assertEqual('*', test_SPRF.requirementType)
-        self.assertEqual('agriculture and life sciences', test_SPRF.requirementValue)
+        self.assertEqual('Agriculture and Life Sciences', test_SPRF.requirementValue)
         self.assertEqual('0', test_SPRF.logicGroup)
 
 if __name__ == '__main__':
