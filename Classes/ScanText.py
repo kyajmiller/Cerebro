3ba90a9e77fc6b9b37fe8aa3221d5edb877b88d0
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-25 16:12:55 -0700

    class to read in text, clean the text, break up resulting lines into array, loop through array and call parsers on each string, return SPRF values

---
+++ Classes/ScanText.py
@@ -0,0 +1,44 @@
+from Classes.CleanText import CleanText
+from Classes.GPA import GPA
+from Classes.Majors import Majors
+from Classes.UACollege import UACollege
+
+
+class ScanText(object):
+    def __init__(self, textToScan):
+        self.textToScan = textToScan
+        self.textLines = []
+
+    def cleanText(self):
+        self.textToScan = CleanText.cleanALLtheText(self.textToScan)
+        return self.textToScan
+
+    def doGPAParser(self, line):
+        parseGPA = GPA(line)
+        if parseGPA.getGPA() != '':
+            print(parseGPA.getGPA())
+            # print(parseGPA.getScholarshipPackageRequirementFormat().getStringValue())
+            # return parseGPA.getScholarshipPackageRequirementFormat()
+
+    def doMajorsParser(self, line):
+        parseMajors = Majors(line, Majors.majorsListForTesting())
+        if parseMajors.getMajors() != '':
+            print(parseMajors.getMajors())
+            # print(parseMajors.getScholarshipPackageRequirementFormat().getStringValue())
+            # return parseMajors.getScholarshipPackageRequirementFormat()
+
+    def doUACollege(self, line):
+        parseUACollege = UACollege(line, UACollege.uaCollegesListForTesting())
+        if parseUACollege.getUACollege() != '':
+            print(parseUACollege.getUACollege())
+            # print(parseUACollege.getScholarshipPackageRequirementFormat().getStringValue())
+            # return parseUACollege.getScholarshipPackageRequirementFormat()
+
+    def processText(self):
+        self.cleanText()
+
+        self.textLines = self.textToScan.split('\n')
+        for line in self.textLines:
+            self.doGPAParser(line)
+            self.doMajorsParser(line)
+            self.doUACollege(line)
