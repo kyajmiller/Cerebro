b392b536c56839a4ae2a0f647429161e9c1e94d7
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-26 09:31:10 -0700

    added code to run tests on scholarshiptest2 and scholarshiptest3

--- Kya Practice Files/ScriptToTestScholarshipTest1.py
+++ Kya Practice Files/ScriptToTestScholarshipTest1.py
@@ -5,3 +5,16 @@ with open('ScholarshipTest1.txt', 'r') as filein:
 
     test = ScanText(wholefile)
     test.processText()
+
+with open('ScholarshipTest2.txt', 'r') as filein:
+    wholefile = ''.join(filein)
+
+    test = ScanText(wholefile)
+    # print(test.cleanText())
+    test.processText()
+
+with open('ScholarshipTest3.txt', 'r') as filein:
+    wholefile = ''.join(filein)
+
+    test = ScanText(wholefile)
+    test.processText()
