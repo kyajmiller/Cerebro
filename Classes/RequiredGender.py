5a6cb02d4e788000560ee9fec4c8bd78b3308345
dmerson <dmerson@email.arizona.edu>
2015-06-26 11:07:51 -0700

    Fixed gender to use non capturing groups

--- Classes/RequiredGender.py
+++ Classes/RequiredGender.py
@@ -4,13 +4,13 @@ from Classes.Parser import Parser
 class RequiredGender(Parser):
     def __init__(self, stringToScan):
         Parser.stringToScan = stringToScan
-        Parser.__init__(self, self.stringToScan, '(\s|\>)[mM]ale|(\s|\>)+[Ff]emale|(\s|\>)+[Ww]omen|(\s|\>)+[Mm]en')
+        Parser.__init__(self, self.stringToScan, '(?:\s|\>)[mM]ale|(?:\s|\>)+[Ff]emale|(?:\s|\>)+[Ww]omen|(?:\s|\>)+[Mm]en')
         self.resultList = []
 
 
     def checkContext(self, contextCriteria):
-        contextChecker = Parser(self.stringToScan.lower(), contextCriteria)
-        return contextChecker.doesMatchExist()
+        return Parser(self.stringToScan.lower(), contextCriteria).doesMatchExist()
+
 
     def getRequiredGender(self):
         if self.doesMatchExist():
@@ -19,4 +19,4 @@ class RequiredGender(Parser):
 
         self.resultList = list(set(self.resultList))
         return self.resultList
-        
+
