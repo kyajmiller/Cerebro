eae5f569234cab86c77fa09abbe16d3710eb8f60
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-26 15:33:55 -0700

    refactor

--- TestGender.py
+++ TestGender.py
@@ -1,19 +1,19 @@
 import unittest
-from Classes.RequiredGender import RequiredGender
+from Classes.Gender import Gender
 
 
 class TestStringMethods(unittest.TestCase):
     def test_RequiredGender(self):
-        testrequiredgender = RequiredGender('This scholarship is for females only')
+        testrequiredgender = Gender('This scholarship is for females only')
         self.assertIsNotNone(testrequiredgender)
         self.assertEqual(testrequiredgender.getRequiredGender(), ['female'])
 
     def test_GenderFailure(self):
-        failrequiredgender = RequiredGender('There is no gender in this string')
+        failrequiredgender = Gender('There is no gender in this string')
         self.assertIsNotNone(failrequiredgender)
         self.assertEqual(failrequiredgender.getRequiredGender(), [])
     def test_betterGender(self):
-        testReq=RequiredGender('This scholarship is for <b>Women </b> only')
+        testReq = Gender('This scholarship is for <b>Women </b> only')
         self.assertIsNotNone(testReq)
         self.assertEqual(True,testReq.doesMatchExist())
 
