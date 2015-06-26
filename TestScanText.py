ac050870333e20db493db7119bc1638af7315628
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-26 16:17:23 -0700

    unit test to verify that the doGPA function returns a SPRF with correct things

--- TestScanText.py
+++ TestScanText.py
@@ -6,8 +6,19 @@ class TestStringMethods(unittest.TestCase):
     def test_ScanText(self):
         with open('ScholarshipTestText1.txt') as filein:
             filein = ''.join(filein)
+
             testscantext = ScanText(filein)
             self.assertIsNotNone(testscantext)
+
+            testscantext.prepareText()
+            testDoGPA = testscantext.doGPAParser()
+            self.assertIsNotNone(testDoGPA)
+            self.assertEqual('1', testDoGPA.attributeId)
+            self.assertEqual('3.0', testDoGPA.requirementValue)
+            self.assertEqual('0', testDoGPA.logicGroup)
+            self.assertEqual('>=', testDoGPA.requirementType)
+            self.assertEqual('0', testDoGPA.scholarshipPackageId)
+
 
 
 if __name__ == '__main__':
