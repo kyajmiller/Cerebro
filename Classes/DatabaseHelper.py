b3bc01b4b990cc7af1bbe15db27191794bf9ddbe
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-29 14:50:37 -0700

    UseOnlyFirstRegex returns True or False depending on if match is found between the regex in the db and the string to scan

--- Classes/DatabaseHelper.py
+++ Classes/DatabaseHelper.py
@@ -1,14 +1,18 @@
 import pyodbc
 from Classes.SUDBConnect import SUDBConnect
+from Classes.Parser import Parser
 
 
 class DatabaseHelper(SUDBConnect):
     @staticmethod
-    def getOnlyOneRegex(attributeId):
+    def UseOnlyFirstRegex(attributeId, stringToScan):
         DB = SUDBConnect()
         rows = DB.getRows(' Select ' + str(attributeId) + ' , RegEx from RegExHelpers')
+        searchCriteria = ''
         if len(rows) >= 1:
-            return rows[0].RegEx
+            searchCriteria = rows[0].RegEx
+        # trueParser = ', '.join(Parser(stringToScan, searchCriteria).getResult())
+        return Parser(stringToScan, searchCriteria).doesMatchExist()
 
     @staticmethod
     def getOnlyOneRegexHelper(attributeId):
