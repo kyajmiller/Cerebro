dd5fa23cf48b10f3182a0634ade35100f4950a43
unknown <zjm203@GODZILLA.sa.arizona.edu>
2015-06-30 10:45:11 -0700

    rewrote the True/False function, everything works as intended but the True+True condition is broken somehow, will return true no matter what

--- Classes/DatabaseHelper.py
+++ Classes/DatabaseHelper.py
@@ -84,29 +84,53 @@ class DatabaseHelper(SUDBConnect):
 
         return doBothMatch
 
-
-'''
     @staticmethod
-    def UseOnlyFirstRegexTrueFalse(attributeId, stringToScan, regexMatch):
+    def useOnlyFirstRegexOrRegexHelperTrueFalse(attributeId, stringToScan, matchRegEx=True, matchRegExHelper=None):
         DB = SUDBConnect()
-        rows = DB.getRows(' Select ' + str(attributeId) + ' , RegEx from RegExHelpers')
-        searchCriteria = ''
+        rows = DB.getRows(' Select ' + str(attributeId) + ' , RegEx, RegExHelper from RegExHelpers')
+        regEx = ''
+        regExHelper = ''
         if len(rows) >= 1:
-            searchCriteria = rows[0].RegEx
-        if regexMatch == Parser(stringToScan, searchCriteria).doesMatchExist():
-            return True
+            regEx = rows[0].RegEx
+            regExHelper = rows[0].RegExHelper
+
+        if matchRegEx == True and not matchRegExHelper:
+            return Parser(stringToScan, regEx).doesMatchExist()
+        elif not matchRegEx and matchRegExHelper == True:
+            return Parser(stringToScan, regExHelper).doesMatchExist()
+        elif matchRegEx == False and not matchRegExHelper:
+            if Parser(stringToScan, regEx).doesMatchExist() == False:
+                return True
+            else:
+                return False
+        elif not matchRegEx and matchRegExHelper == False:
+            if Parser(stringToScan, regExHelper).doesMatchExist() == False:
+                return True
+            else:
+                return False
+        elif matchRegEx == True and matchRegExHelper == True:
+            if Parser(stringToScan, regEx).doesMatchExist() == True and Parser(stringToScan,
+                                                                               regExHelper).doesMatchExist() == True:
+                return True
+            else:
+                return False
+        elif matchRegEx == True and matchRegExHelper == False:
+            if Parser(stringToScan, regEx).doesMatchExist() == True and Parser(stringToScan,
+                                                                               regExHelper).doesMatchExist() == False:
+                return True
+            else:
+                return False
+        elif matchRegEx == False and matchRegExHelper == True:
+            if Parser(stringToScan, regEx).doesMatchExist() == False and Parser(stringToScan,
+                                                                                regExHelper).doesMatchExist() == True:
+                return True
+            else:
+                return False
+        elif matchRegEx == False and matchRegExHelper == False:
+            if Parser(stringToScan, regEx).doesMatchExist() == False and Parser(stringToScan,
+                                                                                regExHelper).doesMatchExist() == False:
+                return True
+            else:
+                return False
         else:
             return False
-
-    @staticmethod
-    def useOnlyOneRegexHelperTrueFalse(attributeId, stringToScan, regexHelperMatch):
-        DB = SUDBConnect()
-        rows = DB.getRows(' Select ' + str(attributeId) + ' , RegExHelper from RegExHelpers')
-        searchCriteria = ''
-        if len(rows) >= 1:
-            searchCriteria = rows[0].RegExHelper
-        if regexHelperMatch == Parser(stringToScan, searchCriteria).doesMatchExist():
-            return True
-        else:
-            return False
-'''
