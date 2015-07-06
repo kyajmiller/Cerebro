f01bf3724664ef742d3439eddc7c1a24973f553a
zjm203 <zjm203@email.arizona.edu>
2015-07-06 14:17:42 -0700

    updated unit tests to reflect change to the argument the class takes

--- TestPullPageLinkTitleDescriptionToDict.py
+++ TestPullPageLinkTitleDescriptionToDict.py
@@ -4,31 +4,25 @@ from Classes.RipPage import RipPage
 
 class TestStringMethods(unittest.TestCase):
     def test_PullPageLinkTitleDescriptionToArrayGetTitle(self):
-        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(
-            RipPage.getPageSource('https://www.google.com/'))
+        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray('https://www.google.com/')
         self.assertIsNotNone(testPullPageLinkTitleDescriptionToArray.getTitle())
         self.assertEqual('Google', testPullPageLinkTitleDescriptionToArray.getTitle())
 
     def test_PullPageLinkTitleDescriptionToArrayGetDescription(self):
-        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(
-            RipPage.getPageSource('https://www.google.com/'))
+        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray('https://www.google.com/')
         self.assertNotEqual('', testPullPageLinkTitleDescriptionToArray.getDescription())
         self.assertGreater(len(testPullPageLinkTitleDescriptionToArray.getDescription()), 10)
 
     def test_PullPageLinkTitleDescriptionToArrayGetPageURL(self):
-        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(
-            RipPage.getPageSource('http://grandcanyon.com'))
+        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray('http://grandcanyon.com')
         self.assertGreater(len(testPullPageLinkTitleDescriptionToArray.getPageURL()), 10)
 
     def test_PullPageLinkTitleDescriptionToArrayGetAllURLsOnPage(self):
-        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(
-            RipPage.getPageSource('http://grandcanyon.com'))
+        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray('http://grandcanyon.com')
         self.assertGreater(len(testPullPageLinkTitleDescriptionToArray.getAllURLsOnPage()), 1)
-        print(testPullPageLinkTitleDescriptionToArray.getAllURLsOnPage())
 
     def test_PullPageLinkTitleDescriptionToArray(self):
-        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(
-            RipPage.getPageSource('http://grandcanyon.com'))
+        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray('http://grandcanyon.com')
         self.assertEqual(4, len(testPullPageLinkTitleDescriptionToArray.arrayTitleDescriptionPageURLLinks()))
         self.assertEqual(testPullPageLinkTitleDescriptionToArray.title,
                          testPullPageLinkTitleDescriptionToArray.arrayTitleDescriptionPageURLLinks()[0])
