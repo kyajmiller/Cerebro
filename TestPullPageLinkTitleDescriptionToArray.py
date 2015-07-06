2b5909e800bcd50e212ecd3921e7b899e553b3a5
zjm203 <zjm203@email.arizona.edu>
2015-07-06 11:03:10 -0700

    getDescription unit tests

--- TestPullPageLinkTitleDescriptionToArray.py
+++ TestPullPageLinkTitleDescriptionToArray.py
@@ -3,10 +3,16 @@ from Classes.PullPageLinkTitleDescriptionToArray import PullPageLinkTitleDescrip
 from Classes.RipPage import RipPage
 
 class TestStringMethods(unittest.TestCase):
-    def test_PullPageLinkTitleDescriptionToArray(self):
+    def test_PullPageLinkTitleDescriptionToArrayGetTitle(self):
         testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(RipPage.getPageSource('https://www.google.com/'))
         self.assertIsNotNone(testPullPageLinkTitleDescriptionToArray.getTitle())
         self.assertEqual('Google', testPullPageLinkTitleDescriptionToArray.getTitle())
+
+    def test_PullPageLinkTitleDescriptionToArrayGetDescription(self):
+        testPullPageLinkTitleDescriptionToArray = PullPageLinkTitleDescriptionToArray(
+            RipPage.getPageSource('https://www.google.com/'))
+        self.assertNotEqual('', testPullPageLinkTitleDescriptionToArray.getDescription())
+        self.assertGreater(len(testPullPageLinkTitleDescriptionToArray.getDescription()), 10)
 
 if __name__ == '__main__':
     unittest.main()
