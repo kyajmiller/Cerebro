b5d45a1b99ea9bd47d67b8fee398cba25ad2515b
zjm203 <zjm203@email.arizona.edu>
2015-07-06 16:01:34 -0700

    turned whole thing into static method, I hate having to declare class instances when there's only one actual function going on, takes too long

--- Classes/InsertScholarshipArrayIntoDatabase.py
+++ Classes/InsertScholarshipArrayIntoDatabase.py
@@ -3,14 +3,12 @@ from Classes.SUDBConnect import SUDBConnect
 
 
 class InsertScholarshipArrayIntoDatabase(object):
-    def __init__(self, scholarshipArray):
-        self.scholarshipArray = scholarshipArray
-        self.url = self.scholarshipArray[0]
-        self.title = self.scholarshipArray[1]
-        self.description = self.scholarshipArray[2]
-        self.db = SUDBConnect()
-
-    def insertScholarshipArrayIntoDatabase(self):
-        self.db.insertUpdateOrDelete(
-            "INSERT INTO dbo.LinkCrawlerHrefs ( QuestionId, LinkUrl, LinkName, LinkDescription, LinkBody, ProcessUsed, IsBadLink, InsertDate, UpdateDate) VALUES  ( 0, '" + self.url + "', '" + self.title + "', '" + self.description + "', '', 0, NULL, GETDATE(), GETDATE())")
+    @staticmethod
+    def doInsert(scholarshipArray):
+        url = scholarshipArray[0]
+        title = scholarshipArray[1]
+        description = scholarshipArray[2]
+        db = SUDBConnect()
+        db.insertUpdateOrDelete(
+            "INSERT INTO dbo.LinkCrawlerHrefs ( QuestionId, LinkUrl, LinkName, LinkDescription, LinkBody, ProcessUsed, IsBadLink, InsertDate, UpdateDate) VALUES  ( 0, '" + url + "', '" + title + "', '" + description + "', '', 0, 0, GETDATE(), GETDATE())")
         return None
