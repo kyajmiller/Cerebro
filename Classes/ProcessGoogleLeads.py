4d95e7213f6ee518e0fe0e01483c525979ac9ac8
zjm203 <zjm203@email.arizona.edu>
2015-07-08 16:30:45 -0700

    just pushing this before I go home, it doesn't do anything yet

---
+++ Classes/ProcessGoogleLeads.py
@@ -0,0 +1,7 @@
+from Classes.GoogleLeads import GoogleLeads
+from Classes.RipPage import RipPage
+from Classes.InsertGoogleLeadArrayToDatabase import InsertGoogleLeadArrayToDatabase
+
+
+class ProcessGoogleLeads(object):
+    def __init__(self, arrayOfGoogleLeads):
