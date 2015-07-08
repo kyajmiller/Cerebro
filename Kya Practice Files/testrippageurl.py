ba7a303a2230b965dba1fba071514cb4cc9abcc3
zjm203 <zjm203@email.arizona.edu>
2015-07-08 16:30:55 -0700

    test stuff pls ignore

---
+++ Kya Practice Files/testrippageurl.py
@@ -0,0 +1,9 @@
+from Classes.RipPage import RipPage
+
+RipPage.getPageSource('http://engineering.berkeley.edu/')
+
+cheese = 'cheese'
+crackers = 'crackers'
+
+cheesecrackers = 'cheese' + crackers
+print(cheesecrackers)
