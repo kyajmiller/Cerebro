5182d67126c52de809bf085ea95d9b6cf0ee02e0
zjm203 <zjm203@email.arizona.edu>
2015-07-09 13:55:57 -0700

    I think this one is done? Seems a bit short. Will do testing

--- Classes/ProcessGoogleLeads.py
+++ Classes/ProcessGoogleLeads.py
@@ -1,7 +1,9 @@
-from Classes.GoogleLeads import GoogleLeads
-from Classes.RipPage import RipPage
 from Classes.InsertGoogleLeadArrayToDatabase import InsertGoogleLeadArrayToDatabase
 
 
 class ProcessGoogleLeads(object):
-    def __init__(self, arrayOfGoogleLeads):
+    def __init__(self, arrayOfGoogleLeadsArrays):
+        self.arrayOfGoogleLeadsArrays = arrayOfGoogleLeadsArrays
+
+        for googleLeadArray in self.arrayOfGoogleLeadsArrays:
+            InsertGoogleLeadArrayToDatabase.doInsert(googleLeadArray)
