9960a127bcfb68409f4f1b31c6b084516fab20bc
zjm203 <zjm203@email.arizona.edu>
2015-07-09 14:55:12 -0700

    refactoring

--- IntegrationTestInsertGoogleLeadArrayToGoogleLeadsDatabase.py
+++ IntegrationTestInsertGoogleLeadArrayToGoogleLeadsDatabase.py
@@ -1,12 +1,12 @@
 import unittest
-from Classes.InsertGoogleLeadArrayToGoogleLeadsDatabase import InsertGoogleLeadArrayToGoogleLeadsDatabase
+from Classes.InsertGoogleLeadArrayIntoGoogleLeadsDatabase import InsertGoogleLeadArrayIntoGoogleLeadsDatabase
 from Classes.SUDBConnect import SUDBConnect
 
 
 class TestStringMethods(unittest.TestCase):
     def test_InsertGoogleLeadsArrayToGoogleLeadsDatabase(self):
         fakeGoogleLeadsArray = ['title', 'link', 'description']
-        InsertGoogleLeadArrayToGoogleLeadsDatabase(fakeGoogleLeadsArray)
+        InsertGoogleLeadArrayIntoGoogleLeadsDatabase(fakeGoogleLeadsArray)
 
         db = SUDBConnect()
         rows = db.getRows("select * from dbo.GoogleLeads where Title='title'")
