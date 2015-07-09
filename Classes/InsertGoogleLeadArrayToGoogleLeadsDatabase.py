4176ab32702c8c0ed0083ed0fc99e530179349e9
zjm203 <zjm203@email.arizona.edu>
2015-07-09 14:42:54 -0700

    removed static method, unnecessary

--- Classes/InsertGoogleLeadArrayToGoogleLeadsDatabase.py
+++ Classes/InsertGoogleLeadArrayToGoogleLeadsDatabase.py
@@ -2,12 +2,11 @@ from Classes.SUDBConnect import SUDBConnect
 
 
 class InsertGoogleLeadArrayToGoogleLeadsDatabase(object):
-    @staticmethod
-    def doInsert(GoogleLeadArray):
+    def __init__(self, googleLeadArray):
+        self.googleLeadArray = googleLeadArray
         db = SUDBConnect()
-        title = GoogleLeadArray[0]
-        link = GoogleLeadArray[1]
-        description = GoogleLeadArray[2]
+        title = self.googleLeadArray[0]
+        link = self.googleLeadArray[1]
+        description = self.googleLeadArray[2]
         db.insertUpdateOrDelete(
             "INSERT INTO dbo.GoogleLeads ( GoogleLeadId, KeyTerm, Title, Link, Description, LinkBody, DateLeadGenerated, DateBodyGenerated) VALUES  ( 0, '', '" + title + "', '" + link + "', '" + description + "', '', GETDATE(), ''")
-        return None
