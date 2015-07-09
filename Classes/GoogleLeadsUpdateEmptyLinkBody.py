1a7b013d1a8da37af9d289f28e072a9d3f2c84bf
zjm203 <zjm203@email.arizona.edu>
2015-07-09 15:29:35 -0700

    updates the GoogleLeads database to fill in the rows with empty LinkBody with the cleaned up page text of the Link

---
+++ Classes/GoogleLeadsUpdateEmptyLinkBody.py
@@ -0,0 +1,21 @@
+from Classes.SUDBConnect import SUDBConnect
+from Classes.RipPage import RipPage
+from Classes.CleanText import CleanText
+
+
+class GoogleLeadsUpdateEmptyLinkBody(object):
+    def __init__(self):
+        self.db = SUDBConnect()
+        self.listOfEmptyLinkBodyLinks = []
+
+        rowsWithEmptyLinkBody = self.db.getRows("select * from dbo.GoogleLeads where ISNULL(LinkBody, '') = ''")
+        if len(rowsWithEmptyLinkBody) >= 1:
+            for row in rowsWithEmptyLinkBody:
+                self.listOfEmptyLinkBodyLinks.append(row.Link)
+
+        if len(self.listOfEmptyLinkBodyLinks) >= 1:
+            for link in self.listOfEmptyLinkBodyLinks:
+                linkbody = RipPage.getPageSource(link)
+                linkbody = CleanText.cleanALLtheText(linkbody)
+                self.db.insertUpdateOrDelete(
+                    "update dbo.GoogleLeads set LinkBody='" + linkbody + "', DateBodyGenerated=GETDATE() where Link='" + link + "'")
