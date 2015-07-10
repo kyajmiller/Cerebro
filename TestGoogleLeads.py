cb478d2b7b8921f166c4c0d804abbf9ef780f569
zjm203 <zjm203@email.arizona.edu>
2015-07-10 12:11:30 -0700

    refactoring

--- TestGoogleLeads.py
+++ TestGoogleLeads.py
@@ -5,12 +5,12 @@ from Classes.GoogleLeads import GoogleLeads
 
 class TestStringMethods(unittest.TestCase):
     def test_GoogleLeads(self):
-        testgoogleleadsarray = GoogleLeads('engineering scholarships').goToGoogleAndGetResults()
+        testgoogleleadsarray = GoogleLeads('engineering scholarships').processSearchResultsAndReturnArrayOfGoogleLeads()
         self.assertIsNotNone(testgoogleleadsarray[0])
         self.assertEqual(3, len(testgoogleleadsarray[0]))
 
     def test_GoogleLeadsElseStatement(self):
-        testgoogleleadsuneventhangs = GoogleLeads('engineering').goToGoogleAndGetResults()
+        testgoogleleadsuneventhangs = GoogleLeads('engineering').processSearchResultsAndReturnArrayOfGoogleLeads()
         self.assertIsNotNone(testgoogleleadsuneventhangs[0])
         self.assertEqual(3, len(testgoogleleadsuneventhangs[0]))
         self.assertTrue(re.search('^https?://', testgoogleleadsuneventhangs[0][1]))
