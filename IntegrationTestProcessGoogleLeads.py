fd78baf317c0d958c5c94a14acbf3936174b6239
zjm203 <zjm203@email.arizona.edu>
2015-07-10 15:28:29 -0700

    Rewriting this unit test because I'm dumb and forgot to push it to git the first time

---
+++ IntegrationTestProcessGoogleLeads.py
@@ -0,0 +1,12 @@
+import unittest
+from Classes.GoogleLeads import GoogleLeads
+from Classes.ProcessGoogleLeads import ProcessGoogleLeads
+from Classes.GoogleLeadsUpdateEmptyLinkBody import GoogleLeadsUpdateEmptyLinkBody
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_ProcessGoogleLeads(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
