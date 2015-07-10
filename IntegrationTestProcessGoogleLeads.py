714e663b24e2e20113820eaf4f144b9aa22d938b
zjm203 <zjm203@email.arizona.edu>
2015-07-10 16:01:34 -0700

    I think it works, I"m not sure, we can't test it well because google is angry.

--- IntegrationTestProcessGoogleLeads.py
+++ IntegrationTestProcessGoogleLeads.py
@@ -2,11 +2,16 @@ import unittest
 from Classes.GoogleLeads import GoogleLeads
 from Classes.ProcessGoogleLeads import ProcessGoogleLeads
 from Classes.GoogleLeadsUpdateEmptyLinkBody import GoogleLeadsUpdateEmptyLinkBody
+from Classes.SUDBConnect import SUDBConnect
 
 
 class TestStringMethods(unittest.TestCase):
     def test_ProcessGoogleLeads(self):
-
+        ProcessGoogleLeads(GoogleLeads('engineering scholarships').processSearchResultsAndReturnArrayOfGoogleLeads())
+        db = SUDBConnect()
+        rows = db.getRows("select * from dbo.GoogleLeads")
+        self.assertGreater(len(rows), 1)
+        # self.assertEqual(len(rows), 100)
 
 if __name__ == '__main__':
     unittest.main()
