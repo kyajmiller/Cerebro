f81ee2b2f2963ffb5b6c9771894f9f0dc29a9792
zjm203 <zjm203@email.arizona.edu>
2015-07-13 11:41:56 -0700

    it says its different but idk how

--- IntegrationTestProcessGoogleLeads.py
+++ IntegrationTestProcessGoogleLeads.py
@@ -8,7 +8,7 @@ from Classes.SUDBConnect import SUDBConnect
 class TestStringMethods(unittest.TestCase):
     def test_ProcessGoogleLeads(self):
         ProcessGoogleLeads(
-            GoogleLeads('engineering scholarships').processSearchResultsAndReturnArrayOfGoogleLeads()[0:9])
+            GoogleLeads('engineering scholarships').processSearchResultsAndReturnArrayOfGoogleLeads())
         db = SUDBConnect()
         rows = db.getRows("select * from dbo.GoogleLeads")
         self.assertGreater(len(rows), 1)
