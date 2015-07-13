efacb40b07aba52a51e8b09654acf315cd9470a5
zjm203 <zjm203@email.arizona.edu>
2015-07-13 16:11:24 -0700

    goes to GrantForward website, inputs search term, gets first page of results, rips results on page, then goes to each page and gets the source website link, puts info in array and returns array of arrays

---
+++ Classes/GrantForwardLeads.py
@@ -0,0 +1,68 @@
+import re
+from selenium import webdriver
+from Classes.CleanText import CleanText
+
+
+class GrantForwardLeads(object):
+    def __init__(self, searchTerm):
+        self.searchTerm = searchTerm
+        self.driver = webdriver.Firefox()
+        self.base_url = 'https://www.grantforward.com/'
+
+        self.arrayOfTitles = []
+        self.arrayOfResultsPagesLinks = []
+        self.arrayOfDescriptionsDiv = []
+        self.arrayOfDescriptionsText = []
+        self.arrayOfWebsiteLinks = []
+        self.arrayOfGrantForwardLeads = []
+        self.arrayOfResultsPageArrays = []
+
+        self.driver.get(self.base_url + '/index')
+        self.driver.find_element_by_id('keyword').clear()
+        self.driver.find_element_by_id('keyword').send_keys(self.searchTerm)
+        self.driver.find_element_by_xpath('//div[2]/button').click()
+        self.driver.implicitly_wait(2)
+
+    def processSearchResultsAndMakeLeadArray(self):
+        self.getTitlesAndLinksFromSearchResults()
+        for i in self.arrayOfResultsPageArrays:
+            title = i[0]
+            resultPageLink = i[1]
+            description = i[2]
+            sourceWebsiteLink = self.goToResultPageAndGetSourceWebsite(resultPageLink)
+
+            singleLeadArray = [title, sourceWebsiteLink, description]
+            self.arrayOfGrantForwardLeads.append(singleLeadArray)
+
+        self.driver.quit()
+
+        for i in self.arrayOfGrantForwardLeads:
+            print(i)
+            # print(self.arrayOfGrantForwardLeads)
+
+    def getTitlesAndLinksFromSearchResults(self):
+        self.arrayOfTitles = self.driver.find_elements_by_xpath("//a[@class = 'grant-url']")
+        self.arrayOfResultsPagesLinks = []
+        for i in self.arrayOfTitles:
+            self.arrayOfResultsPagesLinks.append(i.get_attribute('href'))
+        self.arrayOfDescriptionsDiv = self.driver.find_elements_by_xpath("//div[@class = 'description']")
+        for i in self.arrayOfDescriptionsDiv:
+            self.arrayOfDescriptionsText.append(i.get_attribute('textContent'))
+
+        for i in range(len(self.arrayOfTitles)):
+            title = self.arrayOfTitles[i].text
+            resultPageLink = self.arrayOfResultsPagesLinks[i]
+            description = self.arrayOfDescriptionsText[i].strip()
+            singleResultArray = [title, resultPageLink, description]
+            self.arrayOfResultsPageArrays.append(singleResultArray)
+
+    def goToResultPageAndGetSourceWebsite(self, resultPageLink):
+        self.driver.get(resultPageLink)
+        self.driver.implicitly_wait(2)
+
+        sourceButtonDiv = self.driver.find_element_by_xpath("//a[@class = 'source-link btn btn-warning']")
+        sourceWebsiteLink = sourceButtonDiv.get_attribute('href')
+        return sourceWebsiteLink
+
+
+GrantForwardLeads('engineering').processSearchResultsAndMakeLeadArray()
