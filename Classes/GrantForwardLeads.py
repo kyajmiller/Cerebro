c2051f4967397b4f3a7a0bb62b2a013f42b26770
zjm203 <zjm203@email.arizona.edu>
2015-07-14 12:14:03 -0700

    pulls out everything that dbo.GrantForwardItems wants with the exception of the page source.

--- Classes/GrantForwardLeads.py
+++ Classes/GrantForwardLeads.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from Classes.CleanText import CleanText
+from Classes.RipPage import RipPage
 
 
 class GrantForwardLeads(object):
@@ -24,7 +25,7 @@ class GrantForwardLeads(object):
 
     def processSearchResultsAndMakeLeadArray(self):
         self.getTitlesAndLinksFromSearchResults()
-
+        '''
         isThereNextPage = self.checkIfNextPage()
         pageCount = 2
         while isThereNextPage == True and pageCount <= 10:
@@ -32,7 +33,7 @@ class GrantForwardLeads(object):
             self.getTitlesAndLinksFromSearchResults()
             isThereNextPage = self.checkIfNextPage()
             pageCount += 1
-
+        '''
         for singleArray in self.arrayOfResultsPageArrays:
             self.makeLeadArrayAndAddToGrantForwardLeads(singleArray)
 
@@ -45,34 +46,78 @@ class GrantForwardLeads(object):
         self.arrayOfResultsPagesLinks = []
         for i in self.arrayOfTitles:
             self.arrayOfResultsPagesLinks.append(i.get_attribute('href'))
-        self.arrayOfDescriptionsDiv = self.driver.find_elements_by_xpath("//div[@class = 'description']")
-        for i in self.arrayOfDescriptionsDiv:
-            self.arrayOfDescriptionsText.append(CleanText.cleanALLtheText(i.get_attribute('textContent')))
 
         for i in range(len(self.arrayOfTitles)):
             title = self.arrayOfTitles[i].text
             resultPageLink = self.arrayOfResultsPagesLinks[i]
-            description = self.arrayOfDescriptionsText[i].strip()
-            singleResultArray = [title, resultPageLink, description]
+            singleResultArray = [title, resultPageLink]
             self.arrayOfResultsPageArrays.append(singleResultArray)
 
     def makeLeadArrayAndAddToGrantForwardLeads(self, singleArray):
-        title = singleArray[0]
-        resultPageLink = singleArray[1]
-        description = singleArray[2]
-        sourceWebsiteLink = self.goToResultPageAndGetSourceWebsite(resultPageLink)
+        name = singleArray[0]
+        url = singleArray[1]
+        resultPageInfo = self.goToResultPageAndPullInformation(url)
 
-        singleLeadArray = [title, sourceWebsiteLink, description]
+        keyword = self.searchTerm
+        description = CleanText.cleanALLtheText(resultPageInfo[0])
+        sponsor = CleanText.cleanALLtheText(resultPageInfo[1])
+        amount = resultPageInfo[2]
+        eligibility = CleanText.cleanALLtheText(resultPageInfo[3])
+        submissionInfo = CleanText.cleanALLtheText(resultPageInfo[4])
+        categories = CleanText.cleanALLtheText(resultPageInfo[5])
+        opportunitySourceLink = resultPageInfo[6]
+        # opportunitysourceText = CleanText.cleanALLtheText(RipPage.getPageSource(opportunitySourceLink))
+
+        singleLeadArray = [keyword, url, name, description, sponsor, amount, eligibility, submissionInfo, categories,
+                           opportunitySourceLink]
 
         self.arrayOfGrantForwardLeads.append(singleLeadArray)
 
-    def goToResultPageAndGetSourceWebsite(self, resultPageLink):
+    def goToResultPageAndPullInformation(self, resultPageLink):
         self.driver.get(resultPageLink)
         self.driver.implicitly_wait(2)
+        description = ''
+        sponsor = ''
+        amount = ''
+        eligibility = ''
+        submissionInfo = ''
+        categories = ''
+        opportunitySourceLink = ''
 
-        sourceButtonDiv = self.driver.find_element_by_xpath("//a[@class = 'source-link btn btn-warning']")
-        sourceWebsiteLink = sourceButtonDiv.get_attribute('href')
-        return sourceWebsiteLink
+        if self.checkIfElementExists("//div[@id = 'field-description']/div[@class = 'content-collapsed']"):
+            descriptionDiv = self.driver.find_element_by_xpath(
+                "//div[@id = 'field-description']/div[@class = 'content-collapsed']")
+            description = descriptionDiv.get_attribute('textContent')
+
+        if self.checkIfElementExists("//div[@class = 'sponsor-content']/div/a"):
+            sponsorDiv = self.driver.find_element_by_xpath("//div[@class = 'sponsor-content']/div/a")
+            sponsor = sponsorDiv.get_attribute('textContent')
+
+        if self.checkIfElementExists("//div[@id = 'field-amount_info']/div[@class = 'content-collapsed']"):
+            amountDiv = self.driver.find_element_by_xpath(
+                "//div[@id = 'field-amount_info']/div[@class = 'content-collapsed']")
+            amount = amountDiv.get_attribute('textContent')
+
+        if self.checkIfElementExists("//div[@id = 'field-eligibility']/div[@class = 'content-collapsed']"):
+            eligibilityDiv = self.driver.find_element_by_xpath(
+                "//div[@id = 'field-eligibility']/div[@class = 'content-collapsed']")
+            eligibility = eligibilityDiv.get_attribute('textContent')
+
+        if self.checkIfElementExists("//div[@id = 'field-submission_info']/div[@class = 'content-collapsed']"):
+            submissionInfoDiv = self.driver.find_element_by_xpath(
+                "//div[@id = 'field-submission_info']/div[@class = 'content-collapsed']")
+            submissionInfo = submissionInfoDiv.get_attribute('textContent')
+
+        if self.checkIfElementExists("//div[@id = 'field-subjects']/ul"):
+            categoriesDiv = self.driver.find_element_by_xpath("//div[@id = 'field-subjects']/ul")
+            categories = categoriesDiv.get_attribute('textContent')
+
+        if self.checkIfElementExists("//a[@class = 'source-link btn btn-warning']"):
+            opportunitySourceLinkDiv = self.driver.find_element_by_xpath("//a[@class = 'source-link btn btn-warning']")
+            opportunitySourceLink = opportunitySourceLinkDiv.get_attribute('href')
+
+        resultPageInfo = [description, sponsor, amount, eligibility, submissionInfo, categories, opportunitySourceLink]
+        return resultPageInfo
 
     def checkIfNextPage(self):
         checkNextPage = self.driver.find_elements_by_xpath("(//a[contains(text(), 'Next')])[1]")
@@ -84,3 +129,15 @@ class GrantForwardLeads(object):
     def goToNextPage(self):
         self.driver.find_element_by_xpath("(//a[contains(text(), 'Next')])[1]").click()
         self.driver.implicitly_wait(2)
+
+    def checkIfElementExists(self, xpath):
+        checkElementExists = self.driver.find_elements_by_xpath(xpath)
+        if checkElementExists != []:
+            return True
+        else:
+            return False
+
+
+test = GrantForwardLeads('engineering').processSearchResultsAndMakeLeadArray()
+for i in test[0]:
+    print(i)
