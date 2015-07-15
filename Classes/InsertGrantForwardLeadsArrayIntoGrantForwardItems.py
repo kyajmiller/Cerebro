fbf52927e07d0bda17a5f4c2194540b369d103d0
zjm203 <zjm203@email.arizona.edu>
2015-07-15 15:29:51 -0700

    added function to check if scholarship already exists in database before inserting

--- Classes/InsertGrantForwardLeadsArrayIntoGrantForwardItems.py
+++ Classes/InsertGrantForwardLeadsArrayIntoGrantForwardItems.py
@@ -4,17 +4,27 @@ from Classes.SUDBConnect import SUDBConnect
 class InsertGrantForwardLeadsArrayIntoGrantForwardItems(object):
     def __init__(self, grantForwardLeadArray):
         self.grantForwardLeadArray = grantForwardLeadArray
-        db = SUDBConnect()
-        keyword = self.grantForwardLeadArray[0]
-        url = self.grantForwardLeadArray[1]
-        name = self.grantForwardLeadArray[2]
-        description = self.grantForwardLeadArray[3]
-        sponsor = self.grantForwardLeadArray[4]
-        amount = self.grantForwardLeadArray[5]
-        eligibility = self.grantForwardLeadArray[6]
-        submissionInfo = self.grantForwardLeadArray[7]
-        categories = self.grantForwardLeadArray[8]
-        opportunitySourceLink = self.grantForwardLeadArray[9]
-        opportunitySourceText = self.grantForwardLeadArray[10]
-        db.insertUpdateOrDelete(
-            "insert into dbo.GrantForwardItems (Keyword, Url, Name, Description, Sponsor, Amount, Eligibility, SubmissionInfo, Categories, OpportunitySourceLink, OpportunitySourceText) values (N'" + keyword + "', N'" + url + "', N'" + name + "', N'" + description + "', N'" + sponsor + "', N'" + amount + "', N'" + eligibility + "', N'" + submissionInfo + "', N'" + categories + "', N'" + opportunitySourceLink + "', N'" + opportunitySourceText + "')")
+        self.db = SUDBConnect()
+        self.keyword = self.grantForwardLeadArray[0]
+        self.url = self.grantForwardLeadArray[1]
+        self.name = self.grantForwardLeadArray[2]
+        self.description = self.grantForwardLeadArray[3]
+        self.sponsor = self.grantForwardLeadArray[4]
+        self.amount = self.grantForwardLeadArray[5]
+        self.eligibility = self.grantForwardLeadArray[6]
+        self.submissionInfo = self.grantForwardLeadArray[7]
+        self.categories = self.grantForwardLeadArray[8]
+        self.opportunitySourceLink = self.grantForwardLeadArray[9]
+        self.opportunitySourceText = self.grantForwardLeadArray[10]
+
+        if not self.checkIfAlreadyInDatabase():
+            self.db.insertUpdateOrDelete(
+                "insert into dbo.GrantForwardItems (Keyword, Url, Name, Description, Sponsor, Amount, Eligibility, SubmissionInfo, Categories, OpportunitySourceLink, OpportunitySourceText) values (N'" + self.keyword + "', N'" + self.url + "', N'" + self.name + "', N'" + self.description + "', N'" + self.sponsor + "', N'" + self.amount + "', N'" + self.eligibility + "', N'" + self.submissionInfo + "', N'" + self.categories + "', N'" + self.opportunitySourceLink + "', N'" + self.opportunitySourceText + "')")
+
+    def checkIfAlreadyInDatabase(self):
+        matchingRow = self.db.getRows(
+            "select * from dbo.GrantForwardItems where Keyword='" + self.keyword + "' and Url='" + self.url + "'")
+        if matchingRow != []:
+            return True
+        else:
+            return False
