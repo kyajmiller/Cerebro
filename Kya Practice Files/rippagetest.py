bc199d05e3c976702b97ac016e3e50f2415d3b3d
zjm203 <zjm203@email.arizona.edu>
2015-07-15 10:07:30 -0700

    pulled from master

--- Kya Practice Files/rippagetest.py
+++ Kya Practice Files/rippagetest.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import codecs
 
 from Classes.RipPage import RipPage
 from Classes.CleanText import CleanText
 
 # rippedpage = RipPage.getPageSource('http://webapps.acs.org/findawards/detail.jsp?ContentId=CTP_004520')
 ripped = RipPage.getPageSource('http://webapps.acs.org/findawards/detail.jsp?ContentId=CNBP_031057')
-# dripped = ripped.decode('utf-8', 'ignore')
-cripped = CleanText.cleanALLtheText(ripped)
-# print(cripped)
+arr = bytearray(ripped, "utf-8")
+print(str(arr))
+ripped2 = str(arr)
+print(ripped2)
