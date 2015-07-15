f395ed65e8345d17027748f2724577506c1e7e1c
zjm203 <zjm203@email.arizona.edu>
2015-07-15 10:05:49 -0700

    update

--- Kya Practice Files/rippagetest.py
+++ Kya Practice Files/rippagetest.py
@@ -7,4 +7,5 @@ from Classes.CleanText import CleanText
 # rippedpage = RipPage.getPageSource('http://webapps.acs.org/findawards/detail.jsp?ContentId=CTP_004520')
 ripped = RipPage.getPageSource('http://webapps.acs.org/findawards/detail.jsp?ContentId=CNBP_031057')
 # dripped = ripped.decode('utf-8', 'ignore')
-print(ripped)
+cripped = CleanText.cleanALLtheText(ripped)
+# print(cripped)
