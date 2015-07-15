0147b1f2d8c121b644a7f7d2cfc6b43b2f0056a3
dmerson <dmerson@email.arizona.edu>
2015-07-15 10:04:22 -0700

    Showed conversion needed for unicode.

--- Kya Practice Files/rippagetest.py
+++ Kya Practice Files/rippagetest.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import codecs
 
 from Classes.RipPage import RipPage
 from Classes.CleanText import CleanText
 
 # rippedpage = RipPage.getPageSource('http://webapps.acs.org/findawards/detail.jsp?ContentId=CTP_004520')
 ripped = RipPage.getPageSource('http://webapps.acs.org/findawards/detail.jsp?ContentId=CNBP_031057')
-# dripped = ripped.decode('utf-8', 'ignore')
-print(ripped)
+arr = bytearray(ripped, "utf-8")
+print(str(arr))
+ripped2=str(arr)
+print(ripped2)
+
