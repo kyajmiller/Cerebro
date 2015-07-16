b7c54c77fc764913bbeff1b2d03d6e015579a0e0
zjm203 <zjm203@email.arizona.edu>
2015-07-16 10:06:13 -0700

    unit tests yeah!

---
+++ IntegrationTestGrantForwardRunMajorsList.py
@@ -0,0 +1,19 @@
+import unittest
+from Classes.GrantForwardRunMajorsList import GrantForwardRunMajorsList
+from Classes.SUDBConnect import SUDBConnect
+
+class TestStringMethods(unittest.TestCase):
+    def test_GrantForwardRunMajorsList(self):
+        #set up
+        db = SUDBConnect()
+
+        #run
+        GrantForwardRunMajorsList(isTest=True)
+
+        #test
+        rows = db.getRows("select * from dbo.GrantForwardItems")
+        self.assertGreaterEqual(len(rows), 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
