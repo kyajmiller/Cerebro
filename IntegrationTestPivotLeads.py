63e9a19940a548cb9cc249189e0d203315cdc1d1
zjm203 <zjm203@email.arizona.edu>
2015-07-16 15:49:46 -0700

    unit tests for pivot leads

---
+++ IntegrationTestPivotLeads.py
@@ -0,0 +1,17 @@
+import unittest
+import re
+from Classes.PivotLeads import PivotLeads
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_PivotLeads(self):
+        testPivotLeads = PivotLeads('engineering', isTest=True).processSearchResultsAndMakeLeadArray()
+        self.assertIsNotNone(testPivotLeads)
+        firstArray = testPivotLeads[0]
+        self.assertEqual(len(firstArray), 13)
+        firstArrayLink = firstArray[11]
+        self.assertTrue(re.search('^https?://', firstArrayLink))
+
+
+if __name__ == '__main__':
+    unittest.main()
