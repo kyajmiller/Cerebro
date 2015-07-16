a26cd960b1f5dd2a9b301c136104eedec737bd5d
zjm203 <zjm203@email.arizona.edu>
2015-07-16 09:39:58 -0700

    initial commit

---
+++ Classes/GrantForwardRunMajorsList.py
@@ -0,0 +1,18 @@
+from Classes.GetFastFindMajorsList import GetFastFindMajorsList
+from Classes.GrantForwardLeads import GrantForwardLeads
+from Classes.ProcessGrantForwardLeads import ProcessGrantForwardLeads
+
+
+class GrantForwardRunMajorsList(object):
+    def __init__(self, isTest=False):
+        self.isTest = isTest
+        self.fastFindMajorsList = GetFastFindMajorsList.getList()
+
+        if self.isTest:
+            for major in self.fastFindMajorsList[:4]:
+                majorSpecificGrantForwardLeads = GrantForwardLeads(major).processSearchResultsAndMakeLeadArray()
+                ProcessGrantForwardLeads(majorSpecificGrantForwardLeads)
+        else:
+            for major in self.fastFindMajorsList:
+                majorSpecificGrantForwardLeads = GrantForwardLeads(major).processSearchResultsAndMakeLeadArray()
+                ProcessGrantForwardLeads(majorSpecificGrantForwardLeads)
