0afcefe90ebc1560f93800a9eb99d93597402c78
zjm203 <zjm203@email.arizona.edu>
2015-07-17 10:47:39 -0700

    added optional argument, allows user to pick which table to compare the majors list with, if major already exists in table keywords, delete it and then put it back on at the end; allows program to start on majors that aren't already in the database, and then loops back around

--- Classes/GetFastFindMajorsList.py
+++ Classes/GetFastFindMajorsList.py
@@ -3,12 +3,34 @@ from Classes.SUDBConnect import SUDBConnect
 
 class GetFastFindMajorsList(object):
     @staticmethod
-    def getList():
+    def getList(table=None):
         fastFindMajorsList = []
         db = SUDBConnect()
         rows = db.getRows(
             "select replace( ValueShown, '(' + OtherValuesToCheck + ')', '') as Major from dbo.FastFindLists where AttributeId=417")
         for row in rows:
             fastFindMajorsList.append(row.Major)
+
+        if table == 'GrantForwardItems':
+            rows = db.getRows("SELECT DISTINCT Keyword FROM dbo.GrantForwardItems")
+            existingKeywordsInTable = []
+            for row in rows:
+                existingKeywordsInTable.append(row.Keyword)
+
+            for major in fastFindMajorsList:
+                if major in existingKeywordsInTable:
+                    fastFindMajorsList.remove(major)
+                fastFindMajorsList.append(major)
+
+        if table == 'PivotLeads':
+            rows = db.getRows("SELECT DISTINCT Keyword FROM dbo.PivotLeads")
+            existingKeywordsInTable = []
+            for row in rows:
+                existingKeywordsInTable.append(row.Keyword)
+
+            for major in fastFindMajorsList:
+                if major in existingKeywordsInTable:
+                    fastFindMajorsList.remove(major)
+                fastFindMajorsList.append(major)
 
         return fastFindMajorsList
