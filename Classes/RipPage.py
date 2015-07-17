4d2b12d655baeee19f304dd223405e2ade5b80b1
zjm203 <zjm203@email.arizona.edu>
2015-07-17 09:36:27 -0700

    added try/except statement

--- Classes/RipPage.py
+++ Classes/RipPage.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 
 
 class RipPage(object):
     @staticmethod
     def getPageSource(url):
         driver = webdriver.Firefox()
-        driver.get(url)
-        htmlSource = driver.page_source
+        htmlSource = ''
+        try:
+            driver.get(url)
+            htmlSource = driver.page_source
+        except:
+            htmlSource = 'Page load timeout, source not found.'
         driver.quit()
 
         return htmlSource
