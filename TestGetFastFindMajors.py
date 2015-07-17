ccd5d6c4a82bb18cdedde69896785d0d482b0fcd
zjm203 <zjm203@email.arizona.edu>
2015-07-17 11:04:03 -0700

    test if optional parameter works

--- TestGetFastFindMajors.py
+++ TestGetFastFindMajors.py
@@ -6,6 +6,12 @@ class TestStringMethods(unittest.TestCase):
     def test_GetFastFindMajorsList(self):
         testfastfindmajorslist = GetFastFindMajorsList.getList()
         self.assertGreaterEqual(len(testfastfindmajorslist), 100)
+        print(testfastfindmajorslist)
+
+    def test_GetFastFindMajorsListGrantForwardItems(self):
+        testgrantforwardmajorslist = GetFastFindMajorsList.getList('GrantForwardItems')
+        self.assertGreaterEqual(len(testgrantforwardmajorslist), 100)
+        print(testgrantforwardmajorslist)
 
 
 if __name__ == '__main__':
