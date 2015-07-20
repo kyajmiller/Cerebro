e21f480e940e380243ed5ec7498680f8d631f2c3
zjm203 <zjm203@email.arizona.edu>
2015-07-20 14:00:53 -0700

    update unit tests

--- TestGetFastFindMajors.py
+++ TestGetFastFindMajors.py
@@ -4,14 +4,23 @@ from Classes.GetFastFindMajorsList import GetFastFindMajorsList
 
 class TestStringMethods(unittest.TestCase):
     def test_GetFastFindMajorsList(self):
-        testfastfindmajorslist = GetFastFindMajorsList.getList()
+        testfastfindmajorslist = GetFastFindMajorsList.getDefaultList()
         self.assertGreaterEqual(len(testfastfindmajorslist), 100)
         print(testfastfindmajorslist)
 
     def test_GetFastFindMajorsListGrantForwardItems(self):
-        testgrantforwardmajorslist = GetFastFindMajorsList.getList('GrantForwardItems')
+        testgrantforwardmajorslist = GetFastFindMajorsList.getGrantForwardItemsList()
         self.assertGreaterEqual(len(testgrantforwardmajorslist), 100)
         print(testgrantforwardmajorslist)
+
+    def test_CompareLists(self):
+        testdefault = GetFastFindMajorsList.getDefaultList()
+        testgf = GetFastFindMajorsList.getGrantForwardItemsList()
+        testpivot = GetFastFindMajorsList.getPivotLeadsList()
+
+        print(testdefault)
+        print(testpivot)
+        print(testgf)
 
 
 if __name__ == '__main__':
