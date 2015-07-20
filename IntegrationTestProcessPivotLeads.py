4d92e28fdbd2f7bb83eebf335b317d72ada2f278
zjm203 <zjm203@email.arizona.edu>
2015-07-20 14:14:04 -0700

    unit test

---
+++ IntegrationTestProcessPivotLeads.py
@@ -0,0 +1,16 @@
+import unittest
+from Classes.PivotLeads import PivotLeads
+from Classes.ProcessPivotLeads import ProcessPivotLeads
+from Classes.SUDBConnect import SUDBConnect
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_ProcessPivotLeads(self):
+        ProcessPivotLeads(PivotLeads('engineering', isTest=True).processSearchResultsAndMakeLeadArray())
+        db = SUDBConnect()
+        rows = db.getRows("select * from dbo.PivotLeads where Keyword='engineering'")
+        self.assertGreaterEqual(len(rows), 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
