86db4f762a3670a8a43342a07040f2b99bf39fbf
zjm203 <zjm203@email.arizona.edu>
2015-07-20 16:21:36 -0700

    run pivot leads unit test

---
+++ IntegrationTestPivotLeadsRunMajorsList.py
@@ -0,0 +1,31 @@
+import unittest
+from Classes.PivotLeadsRunMajorsList import PivotLeadsRunMajorsList
+from Classes.SUDBConnect import SUDBConnect
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_PivotLeadsRunMajorsList(self):
+        # set up
+        db = SUDBConnect()
+
+        # run
+        PivotLeadsRunMajorsList(isTest=True)
+
+        # test
+        rows = db.getRows("select * from dbo.PivotLeads")
+        self.assertGreaterEqual(len(rows), 100)
+
+    def test_PivotLeadsRunMajorsListActuallyDoIt(self):
+        # set up
+        db = SUDBConnect()
+
+        # run
+        PivotLeadsRunMajorsList()
+
+        # test
+        rows = db.getRows("select * from dbo.PivotLeads")
+        self.assertGreaterEqual(len(rows), 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
