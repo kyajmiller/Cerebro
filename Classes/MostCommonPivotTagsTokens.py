1ce243498e825e65d7d93eac084b3c498c9da8e5
zjm203 <zjm203@email.arizona.edu>
2015-07-21 15:49:19 -0700

    get most common tokens

---
+++ Classes/MostCommonPivotTagsTokens.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk import FreqDist
+from Classes.GetPivotTagsConcatenateAbstractEligibility import GetPivotTagsConcatenateAbstractEligibility
+from Classes.TokenizeOnWhitespacePunctuation import TokenizeOnWhitespacePunctuation
+
+
+class MostCommonPivotTagsTokens(object):
+    def __init__(self, topNumber):
+        self.topNumber = topNumber
+        self.pivotTagsData = GetPivotTagsConcatenateAbstractEligibility.getList()
+        self.tokens = []
+
+    def tokenizeData(self):
+        for data in self.pivotTagsData:
+            unigramsBigrams = TokenizeOnWhitespacePunctuation(data).getBothUnigramsBigrams()
+            for token in unigramsBigrams:
+                self.tokens.append(token)
+
+    def getMostCommon(self):
+        self.tokenizeData()
+        mostCommon = FreqDist(self.tokens).most_common(self.topNumber)
+        return mostCommon
+
+    '''
+    @staticmethod
+    def getMostCommonTokens():
+        pivotTagsAbstractEligibilityCombos = GetPivotTagsConcatenateAbstractEligibility.getList()
+        for aeCombo in pivotTagsAbstractEligibilityCombos:
+    '''
