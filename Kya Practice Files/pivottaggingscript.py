7091a06996ea0183415ea491759f4cb9a2a6fd60
zjm203 <zjm203@email.arizona.edu>
2015-07-21 11:27:43 -0700

    nvm this thing sucks

---
+++ Kya Practice Files/pivottaggingscript.py
@@ -0,0 +1,28 @@
+from Classes.SUDBConnect import SUDBConnect
+
+
+def getEmptyTagAbstracts():
+    db = SUDBConnect()
+
+    emptyTagRows = db.getRows("select * from dbo.PivotTags where ISNULL(Tag, '') = ''")
+    emptyTagAbstracts = []
+    for row in emptyTagRows:
+        emptyTagAbstracts.append(row.Abstract)
+
+    return emptyTagAbstracts
+
+
+def insertTag(abstract, tag):
+    db = SUDBConnect()
+    db.insertUpdateOrDelete("update dbo.PivotTags set Tag='" + tag + "' where Abstract='" + abstract + "'")
+
+
+abstractsList = getEmptyTagAbstracts()
+print(abstractsList)
+
+'''
+for abstract in abstractsList:
+    print(abstract)
+    tag = input("Tag: ")
+    insertTag(abstract, tag)
+'''
