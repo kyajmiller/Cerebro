ac2ac8872ad7419621ece50973d117223f7a9fb6
zjm203 <zjm203@email.arizona.edu>
2015-07-22 10:38:45 -0700

    update unit test

--- TestGetPivotTagsConcatenateTitleAbstractEligibility.py
+++ TestGetPivotTagsConcatenateTitleAbstractEligibility.py
@@ -1,21 +1,22 @@
 import unittest
-from Classes.GetPivotTagsConcatenateAbstractEligibility import GetPivotTagsConcatenateAbstractEligibility
+from Classes.GetPivotTagsConcatenateTitleAbstractEligibility import GetPivotTagsConcatenateTitleAbstractEligibility
 from Classes.SUDBConnect import SUDBConnect
 from Classes.CleanText import CleanText
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_GetPivotTagsConcatenateAbstractEligibility(self):
+    def test_GetPivotTagsConcatenateTitleAbstractEligibility(self):
         # set up
         db = SUDBConnect()
-        testConcatenatAbstractEligibility = GetPivotTagsConcatenateAbstractEligibility.getList()
+        testConcatenatAbstractEligibility = GetPivotTagsConcatenateTitleAbstractEligibility.getList()
         firstItem = testConcatenatAbstractEligibility[0]
 
         # test
         rows = db.getRows("select * from dbo.PivotTags")
+        title = rows[0].Name
         abstract = rows[0].Abstract
         eligibility = rows[0].Eligibility
-        combo = '%s %s' % (abstract, eligibility)
+        combo = '%s %s %s' % (title, abstract, eligibility)
         combo = CleanText.cleanALLtheText(combo)
 
         self.assertEqual(firstItem, combo)
