b37b5fbe7f8a8a26450d841dbb960ab056a1d0e4
zjm203 <zjm203@email.arizona.edu>
2015-07-24 15:31:13 -0700

    unit tests

--- TestClassifyFundingTypeKeywordBased.py
+++ TestClassifyFundingTypeKeywordBased.py
@@ -9,6 +9,12 @@ class TestStringMethods(unittest.TestCase):
         predictedtag = testclassify.returnPredictedTags()[0]
         self.assertEqual('Scholarship', predictedtag)
 
+    def test_returnPredictedTagsFakeOpportunityAnother(self):
+        fakeopportunity = ['this is a fake opportunity for a grant']
+        testclassify = ClassifyFundingTypeKeywordBased(fakeopportunity)
+        predictedtag = testclassify.returnPredictedTags()[0]
+        self.assertEqual('Grant', predictedtag)
+
 
 if __name__ == '__main__':
     unittest.main()
