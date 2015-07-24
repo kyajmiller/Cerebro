950eb8ab4d9b408bd6050edb34bd1d0b491a3a3a
zjm203 <zjm203@email.arizona.edu>
2015-07-24 11:52:44 -0700

    class to compute accuracy, should eventually be expanded to include precision, recall and f1

---
+++ Classes/ComputeAccuracy.py
@@ -0,0 +1,21 @@
+class ComputeAccuracy(object):
+    def __init__(self, expectedResultsList, predictedResultsList):
+        self.predictedResultsList = predictedResultsList
+        self.expectedResultsList = expectedResultsList
+        self.matches = 0
+        self.total = 0
+        self.accuracy = ''
+
+    def getStats(self):
+        for i in range(len(self.predictedResultsList)):
+            self.total += 1
+            expected = self.expectedResultsList[i]
+            actual = self.predictedResultsList[i]
+
+            if expected == actual:
+                self.matches += 1
+
+    def calculateAccuracy(self):
+        self.getStats()
+        self.accuracy = self.matches / self.total
+        return self.accuracy
