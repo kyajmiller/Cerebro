7931d74e1568daad2ceb9950a5987edfaf2d2e71
zjm203 <zjm203@email.arizona.edu>
2015-07-24 11:07:18 -0700

    unit test on fake opportunity

---
+++ TestClassifyFundingTypeKeywordBased.py
@@ -0,0 +1,15 @@
+import unittest
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
+from Classes.GetPivotTagsTitleAbstractEligibility import GetPivotTagsTitleAbstractEligibility
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_returnPredictedTags(self):
+        fakeopportunity = ['this is a fake opportunity for a scholarship']
+        testclassify = ClassifyFundingTypeKeywordBased(fakeopportunity)
+        predictedtag = testclassify.returnPredictedTags()[0]
+        self.assertEqual('Scholarship', predictedtag)
+
+
+if __name__ == '__main__':
+    unittest.main()
