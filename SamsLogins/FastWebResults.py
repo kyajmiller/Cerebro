5165bc7a2e8c79d82098dd65bc011e63437a335d
don-E Merson <dmerson@email.arizona.edu>
2015-07-28 12:30:18 -0700

    FastWebClass runs without errors

--- SamsLogins/FastWebResults.py
+++ SamsLogins/FastWebResults.py
@@ -1,4 +1,22 @@
+import SamsLogins.FastWebClass as FastWebClass
 
 class FastWebResults(object):
-    def __init__(self, searchTerm):
+    def __init__(self, searchTerms, names,providers,awards):
         self.FastWebPages=[]
+        self.searchTerms=searchTerms
+        self.names=names
+        self.providers=providers
+        self.awards=awards
+
+    def addScholarshipToObject(self):
+        for page in range(0,len(self.searchTerms)):
+            print(page)
+            #newScholarship =  FastWebClass(self.searchTerms[page])
+            #print(FastWebClass(self.searchTerms[page]))
+searchTerms=["test","test2"]
+names=[]
+providers=[]
+awards=[]
+
+fastResults= FastWebResults(searchTerms,names,providers,awards)
+fastResults.addScholarshipToObject()
