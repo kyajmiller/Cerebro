d10969461da94cd83a4871e8e8f5aaef339ae0b4
zjm203 <zjm203@email.arizona.edu>
2015-07-28 15:53:46 -0700

    this works for now but it really needs work

--- Classes/DueDate.py
+++ Classes/DueDate.py
@@ -5,7 +5,7 @@ from Classes.Parser import Parser
 class DueDate(Parser):
     def __init__(self, stringToScan):
         self.stringToScan = stringToScan
-        self.dateRegex = '((January|February|March|April|May|June|July|August|September|October|November|December)|([01][0-9]\/))(\s[123]?[0-9](st|nd|rd|th)?[,\/]?(\d{2})?\s)?(\d{4})?'
+        self.dateRegex = '(((January|February|March|April|May|June|July|August|September|October|November|December)|([01][0-9]\/))(\s[123]?[0-9](st|nd|rd|th)?[,\/]?(\d{2})?\s)?(\d{4})?)'
         Parser.__init__(self, self.stringToScan, self.dateRegex)
 
         self.resultList = []
@@ -19,7 +19,7 @@ class DueDate(Parser):
         if self.checkContext(
                 'due\sdate|application\swindow|deadline|dead\sline|close\sdate|close') and self.doesMatchExist():
             for i in self.getResult():
-                self.resultList.append(i)
+                self.resultList.append(i[0])
 
         self.resultList = list(set(self.resultList))
         self.dueDate = self.resultList
