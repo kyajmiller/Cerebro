fdacf041f63faeeee80d320e741f3f65703b6e05
zjm203 <zjm203@email.arizona.edu>
2015-07-29 16:01:29 -0700

    unit tests yay

--- TestDueDate.py
+++ TestDueDate.py
@@ -8,12 +8,20 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNotNone(testDueDate)
         self.assertEqual('1 August 2015', testDueDate.getDueDate())
 
-    def test_formatDate(self):
+    def test_formatDateMonthDayYear(self):
         testDueDate = DueDate('The application window closes on August 1, 2015.')
         self.assertIsNotNone(testDueDate)
         dateToFormat = 'August 1, 2015'
         formattedDate = testDueDate.formatDateMonthDayYear(dateToFormat)
         self.assertEqual('1 August 2015', formattedDate)
+
+    def test_formatDateNumbers(self):
+        testDueDate = DueDate('The application window closes on August 1, 2015.')
+        self.assertIsNotNone(testDueDate)
+        dateToFormat = '08/01/2015'
+        formattedDate = testDueDate.formatDateNumbers(dateToFormat)
+        self.assertEqual('1 August 2015', formattedDate)
+
 
 if __name__ == '__main__':
     unittest.main()
