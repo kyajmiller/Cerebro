3ddab697ba02d989126111730f2ca0a3ca04171d
zjm203 <zjm203@email.arizona.edu>
2015-07-29 13:12:53 -0700

    added due date extraction, gpa and due date extractors have own functions

--- Classes/PopulatePivotLeadRequirements.py
+++ Classes/PopulatePivotLeadRequirements.py
@@ -3,6 +3,7 @@ from Classes.SUDBConnect import SUDBConnect
 from Classes.GetFastFindMajorsList import GetFastFindMajorsList
 from Classes.PivotLeadsGetDatabaseInfo import PivotLeadsGetDatabaseInfo
 from Classes.GPA import GPA
+from Classes.DueDate import DueDate
 
 
 class PopulatePivotLeadRequirements(object):
@@ -19,23 +20,46 @@ class PopulatePivotLeadRequirements(object):
             pivotLeadsDatabaseInfo = PivotLeadsGetDatabaseInfo(major, self.tag)
             listOfPivotLeadsIds = pivotLeadsDatabaseInfo.getPivotLeadId()
             listOfConcatenatedAbstractEligibilities = pivotLeadsDatabaseInfo.getListStringConcatendatedAbstractEligibility()
+            listOfSourceTexts = pivotLeadsDatabaseInfo.getSourceText()
 
             for i in range(len(listOfPivotLeadsIds)):
                 pivotLeadId = listOfPivotLeadsIds[i]
+
                 abstractEligibility = listOfConcatenatedAbstractEligibilities[i].strip()
-                tokenizedSentences = self.tokenizeAbstractEligibilityIntoSentences(abstractEligibility)
+                abstractEligibilitySentences = self.tokenizeAbstractEligibilityIntoSentences(abstractEligibility)
 
-                gpa = []
-                dueDate = ''
+                sourceText = listOfSourceTexts[i]
+                sourceTextSentences = self.tokenizeAbstractEligibilityIntoSentences(sourceText)
 
-                for sentence in tokenizedSentences:
-                    maybeGPA = GPA(sentence).getGPA()
-                    if maybeGPA != '':
-                        gpa.append(maybeGPA)
+                gpa = self.getGPAFromAbstractEligibility(abstractEligibilitySentences)
+                dueDate = self.getDueDateFromSourceText(sourceTextSentences)
 
-                gpa = ', '.join(gpa)
+                print(pivotLeadId, gpa, dueDate)
 
-                print(pivotLeadId, gpa)
+    def getGPAFromAbstractEligibility(self, abstractEligibilitySentences):
+        gpa = []
+        for sentence in abstractEligibilitySentences:
+            maybeGPA = GPA(sentence).getGPA()
+            if maybeGPA != '':
+                gpa.append(maybeGPA)
+
+        gpa = list(set(gpa))
+        gpa = ', '.join(gpa)
+
+        return gpa
+
+    def getDueDateFromSourceText(self, sourceTextSentences):
+        dueDates = []
+
+        for sentence in sourceTextSentences:
+            maybeDueDate = DueDate(sentence).getDueDate()
+            if maybeDueDate != '':
+                dueDates.append(maybeDueDate)
+
+        dueDates = list(set(dueDates))
+        dueDates = ', '.join(dueDates)
+
+        return dueDates
 
     def tokenizeAbstractEligibilityIntoSentences(self, abstractEligibility):
         sentences = self.sentenceTokenizer.tokenize(abstractEligibility)
