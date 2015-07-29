cc1c317d23e453f0154fa1ab71920888db6b127b
zjm203 <zjm203@email.arizona.edu>
2015-07-29 15:19:16 -0700

    added unit test to check formatDateMonthDayYear

--- TestDueDate.py
+++ TestDueDate.py
@@ -8,6 +8,12 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNotNone(testDueDate)
         self.assertEqual('August 1, 2015', testDueDate.getDueDate())
 
+    def test_formatDate(self):
+        testDueDate = DueDate('The application window closes on August 1, 2015.')
+        self.assertIsNotNone(testDueDate)
+        dateToFormat = 'August 1, 2015'
+        formattedDate = testDueDate.formatDateMonthDayYear(dateToFormat)
+        print(formattedDate)
 
 if __name__ == '__main__':
     unittest.main()
