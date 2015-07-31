52437d37ea745427429a824873725830f837d0ec
zjm203 <zjm203@email.arizona.edu>
2015-07-31 11:44:06 -0700

    downloads punkt module from nltk

--- Programs/Pivot/RunPivotRequirements.py
+++ Programs/Pivot/RunPivotRequirements.py
@@ -1,6 +1,8 @@
 import sys
+import nltk
 from LoopPopulatePivotLeadRequirementsOverMajorsList import LoopPopulatePivotLeadRequirementsOverMajorsList
 
+nltk.download('punkt')
 argvNumberOfMajors = sys.argv[1]
 if sys.argv[1] != 'All':
     argvNumberOfMajors = int(argvNumberOfMajors)
