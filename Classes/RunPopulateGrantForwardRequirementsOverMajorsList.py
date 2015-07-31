ebd2ba561662bbef53e88a0f0e37a117c6089b0e
zjm203 <zjm203@email.arizona.edu>
2015-07-31 09:56:28 -0700

    run the thing

---
+++ Classes/RunPopulateGrantForwardRequirementsOverMajorsList.py
@@ -0,0 +1,21 @@
+from Classes.PopulateGrantForwardRequirements import PopulateGrantForwardRequirements
+from Classes.GrantForwardItemsGetDatabaseInfo import GrantForwardItemsGetDatabaseInfo
+
+
+class RunPopulateGrantForwardRequirementsOverMajorsList(object):
+    def __init__(self, numberOfMajors='All'):
+        self.numberOfMajors = numberOfMajors
+
+    def getMajorsList(self):
+        completeMajorsList = GrantForwardItemsGetDatabaseInfo.getKeywords(tag='Scholarship')
+
+        if self.numberOfMajors == 'All':
+            majorsSlice = completeMajorsList[:]
+        else:
+            majorsSlice = completeMajorsList[:self.numberOfMajors]
+
+        return majorsSlice
+
+    def run(self):
+        listOfMajors = self.getMajorsList()
+        PopulateGrantForwardRequirements(listOfMajors)
