f48e99f140569e0dcc7c01ff7609483a3183e2e4
zjm203 <zjm203@email.arizona.edu>
2015-07-31 15:35:54 -0700

    reformat

--- Classes/SUDBConnect.py
+++ Classes/SUDBConnect.py
@@ -5,24 +5,29 @@ class SUDBConnect(object):
         self.server = server
         self.database = database
         connectionString = r'Driver={SQL Server};Server=%s;Database=%s;Trusted_Connection=yes;' % (
-        self.server, self.database)
+            self.server, self.database)
         self.cnxn = pyodbc.connect(connectionString)
 
-    def getRows(self,sql):
+    def getRows(self, sql):
         cursor = self.cnxn.cursor()
         cursor.execute(sql)
-        rows= cursor.fetchall()
+        rows = cursor.fetchall()
         return rows
-    def insertUpdateOrDelete(self,sql):
+
+    def insertUpdateOrDelete(self, sql):
         cursor = self.cnxn.cursor()
         cursor.execute(sql)
         cursor.commit()
         pass
+
     def getAllTestCases(self):
         return self.getRows("Select * from DepartmentTestCases")
-    def getNumberOfTestCases(self,numberToGet):
+
+    def getNumberOfTestCases(self, numberToGet):
         return self.getRows("Select top " + str(numberToGet) + " * from DepartmentTestCases")
-    def getNumberOfRandomTestCases(self,numberToGet):
+
+    def getNumberOfRandomTestCases(self, numberToGet):
         return self.getRows("Select top " + str(numberToGet) + " * from DepartmentTestCases order by newid()")
-    def insertIntoJustTests(self,valToInsert):
+
+    def insertIntoJustTests(self, valToInsert):
         self.insertUpdateOrDelete("insert into justtests(testvalue) values ('" + str(valToInsert) + "')")
