6fc4ccd1898e92a162b3f9f74f1693cec41b8401
zjm203 <zjm203@email.arizona.edu>
2015-07-31 14:49:12 -0700

    update to match the one in GrantForward

--- Classes/RunGrantForwardRequirements.py
+++ Classes/RunGrantForwardRequirements.py
@@ -1,8 +1,18 @@
 import sys
 import nltk
+import ast
 from Classes.LoopPopulateGrantForwardRequirementsOverMajorsList import \
     LoopPopulateGrantForwardRequirementsOverMajorsList
 
 nltk.download('punkt')
-argvNumberOfMajors = sys.argv[1]
-LoopPopulateGrantForwardRequirementsOverMajorsList(argvNumberOfMajors).run()
+
+argvMajors = ''
+if sys.argv[1] == 'All':
+    argvMajors = sys.argv[1]
+else:
+    inputList = ast.literal_eval(sys.argv[1])
+    if type(inputList) == list:
+        argvMajors = inputList
+        print(argvMajors)
+
+LoopPopulateGrantForwardRequirementsOverMajorsList(argvMajors).run()
