e966a710d40e9338c61af56a9091b27f9dc6ba53
zjm203 <zjm203@email.arizona.edu>
2015-07-31 10:43:30 -0700

    test command line file, should print the passed argv

--- Kya Practice Files/commandLineTestRun.py
+++ Kya Practice Files/commandLineTestRun.py
@@ -1,4 +1,10 @@
+import sys
+
 class PrintStuff(object):
     def __init__(self, thingToPrint):
         self.thingToPrint = thingToPrint
         print(self.thingToPrint)
+
+
+printStuffArg = sys.argv[0]
+PrintStuff(printStuffArg)
