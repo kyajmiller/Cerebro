03317463daa072581510ca970c17971778df66d7
zjm203 <zjm203@email.arizona.edu>
2015-08-04 12:08:56 -0700

    process scholarsite leads

---
+++ Classes/ProcessScholarsiteLeads.py
@@ -0,0 +1,10 @@
+from Classes.ScholarsiteLeads import ScholarsiteLeads
+from Classes.InsertScholarsiteLeadsArrayIntoScholarsiteLeadsDB import InsertScholarsiteLeadsArrayIntoScholarsiteLeadsDB
+
+
+class ProcessScholarsiteLeads(object):
+    @staticmethod
+    def getScholarsiteLeadsAndInsertIntoDB():
+        scholarsiteLeadsArrays = ScholarsiteLeads().processResultsPages()
+        for leadArray in scholarsiteLeadsArrays:
+            InsertScholarsiteLeadsArrayIntoScholarsiteLeadsDB(leadArray)
