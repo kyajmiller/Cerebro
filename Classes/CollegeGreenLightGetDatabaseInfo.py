79da980317e22f64e2eee9cd93746e42d76ae4aa
zjm203 <zjm203@email.arizona.edu>
2015-08-10 11:36:45 -0700

    get database info from CollegeGreenLightLeads

---
+++ Classes/CollegeGreenLightGetDatabaseInfo.py
@@ -0,0 +1,48 @@
+from Classes.SUDBConnect import SUDBConnect
+
+
+class CollegeGreenLightGetDatabaseInfo(object):
+    def __init__(self):
+        self.db = SUDBConnect()
+
+    def getDescriptions(self):
+        descriptions = []
+
+        rows = self.db.getRows("select Description from dbo.CollegeGreenLightLeads")
+        for row in rows:
+            descriptions.append(row.Description)
+
+        return descriptions
+
+    def getSourceTexts(self):
+        sourceTexts = []
+
+        rows = self.db.getRows("select SourceText from dbo.CollegeGreenLightLeads")
+        for row in rows:
+            sourceTexts.append(row.SourceText)
+
+        return sourceTexts
+
+    def getCollegeGreenLightLeadsIds(self):
+        collegeGreenLightLeadIds = []
+
+        rows = self.db.getRows("select CollegeGreenLightLeadId from dbo.CollegeGreenLightLeads")
+        for row in rows:
+            collegeGreenLightLeadIds.append(row.CollegeGreenLightLeadId)
+
+        return collegeGreenLightLeadIds
+
+    def getConcatenatedDescriptionSourceText(self):
+        listConcatenatedItems = []
+
+        descriptions = self.getDescriptions()
+        sourceTexts = self.getSourceTexts()
+
+        for i in range(len(descriptions)):
+            description = descriptions[i]
+            sourceText = sourceTexts[i]
+
+            concatenatedItem = '%s %s' % (description, sourceText)
+            listConcatenatedItems.append(concatenatedItem)
+
+        return listConcatenatedItems
