a25967b567abb1938a25a1623972c8cd73571dc4
zjm203 <zjm203@email.arizona.edu>
2015-08-11 13:04:34 -0700

    update class to work with super class

--- Classes/PopulateFatomeiLeadRequirements.py
+++ Classes/PopulateFatomeiLeadRequirements.py
@@ -1,16 +1,13 @@
-import nltk.data
-from Classes.SUDBConnect import SUDBConnect
+from Classes.PopulateRequirements import PopulateRequirements
 from Classes.FatomeiLeadsGetDatabaseInfo import FatomeiLeadsGetDatabaseInfo
-from Classes.GPA import GPA
-from Classes.Majors import Majors
-from Classes.GetFastFindMajorsList import GetFastFindMajorsList
 
 
-class PopulateFatomeiLeadRequirements(object):
+class PopulateFatomeiLeadRequirements(PopulateRequirements):
     def __init__(self):
-        self.db = SUDBConnect()
+        self.tableName = 'FatomeiLeadRequirements'
+        self.columnNames = ['FatomeiLeadId', 'AttributeId', 'AttributeValue']
+        PopulateRequirements.__init__(self, self.tableName, self.columnNames)
         self.tag = 'Scholarship'
-        self.sentenceTokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
     def loopThroughLeadsAndDoStuff(self):
         fatomeiLeadsDatabaseInfo = FatomeiLeadsGetDatabaseInfo(self.tag)
@@ -23,69 +20,10 @@ class PopulateFatomeiLeadRequirements(object):
             descriptionSourceText = listConcatenatedDescriptionsSourceText[i]
             descriptionSourceTextSentences = self.tokenizeIntoSentences(descriptionSourceText)
 
-            gpa = self.getGPAFromDescriptionSourceText(descriptionSourceTextSentences)
-            majors = self.getMajorsFromDescriptionSourceText(descriptionSourceTextSentences)
+            gpa = self.getGPAFromSentences(descriptionSourceTextSentences)
+            majors = self.getMajorFromSentences(descriptionSourceTextSentences)
 
             self.doDatabaseInserts(fatomeiLeadId, gpa, majors)
-
-    def tokenizeIntoSentences(self, stringToTokenize):
-        sentences = self.sentenceTokenizer.tokenize(stringToTokenize)
-        return sentences
-
-    def getGPAFromDescriptionSourceText(self, descriptionSourceTextSentences):
-        gpa = []
-
-        for sentence in descriptionSourceTextSentences:
-            if len(sentence) <= 1000:
-                maybeGPA = GPA(sentence).getGPA()
-                if maybeGPA != '':
-                    gpa.append(maybeGPA)
-
-        gpa = list(set(gpa))
-        gpa = ', '.join(gpa)
-
-        return gpa
-
-    def getMajorsFromDescriptionSourceText(self, descriptionSourceTextSentences):
-        majors = []
-
-        majorsList = GetFastFindMajorsList.getDefaultList()
-        majorsList = [major.lower() for major in majorsList]
-        majorsList = list(set(majorsList))
-
-        majorsListRegex = '|'.join(majorsList)
-
-        for sentence in descriptionSourceTextSentences:
-            if len(sentence) <= 1000:
-                maybeMajor = Majors(sentence, majorsListRegex).getMajors()
-                if maybeMajor != '':
-                    majors.append(maybeMajor)
-
-        majors = list(set(majors))
-        majors = ', '.join(majors)
-
-        return majors
-
-    def doDatabaseInserts(self, fatomeiLeadId, gpa, major):
-        if major != '':
-            self.insertMajorIntoFatomeiLeadRequirements(fatomeiLeadId, major)
-
-        if gpa != '':
-            self.insertGPAIntoFatomeiLeadRequirements(fatomeiLeadId, gpa)
-
-    def insertMajorIntoFatomeiLeadRequirements(self, fatomeiLeadId, major):
-        attributeId = '417'
-        attributeValue = major
-
-        self.db.insertUpdateOrDelete(
-            "insert into dbo.FatomeiLeadRequirements (FatomeiLeadId, AttributeId, AttributeValue) values ('" + fatomeiLeadId + "', '" + attributeId + "', '" + attributeValue + "')")
-
-    def insertGPAIntoFatomeiLeadRequirements(self, fatomeiLeadId, gpa):
-        attributeId = '1'
-        attributeValue = gpa
-
-        self.db.insertUpdateOrDelete(
-            "insert into dbo.FatomeiLeadRequirements (FatomeiLeadId, AttributeId, AttributeValue) values ('" + fatomeiLeadId + "', '" + attributeId + "', '" + attributeValue + "')")
 
 
 PopulateFatomeiLeadRequirements().loopThroughLeadsAndDoStuff()
