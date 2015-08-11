1993ad850cefe6b5404d1fd301cd2c756b91b3aa
zjm203 <zjm203@email.arizona.edu>
2015-08-11 13:00:16 -0700

    updated to work with the nice PopulateRequirements super class

--- Classes/PopulateCollegeGreenLightRequirements.py
+++ Classes/PopulateCollegeGreenLightRequirements.py
@@ -1,15 +1,13 @@
-import nltk.data
-from Classes.SUDBConnect import SUDBConnect
+from Classes.PopulateRequirements import PopulateRequirements
 from Classes.CollegeGreenLightLeadsGetDatabaseInfo import CollegeGreenLightLeadsGetDatabaseInfo
-from Classes.GPA import GPA
-from Classes.Majors import Majors
-from Classes.GetFastFindMajorsList import GetFastFindMajorsList
 
 
-class PopulateCollegeGreenLightRequirements(object):
+class PopulateCollegeGreenLightRequirements(PopulateRequirements):
     def __init__(self):
-        self.db = SUDBConnect()
-        self.sentenceTokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        self.columnNames = ['CollegeGreenLightLeadId', 'AttributeId', 'AttributeValue']
+        self.tableName = 'CollegeGreenLightRequirements'
+        PopulateRequirements.__init__(self, self.tableName, self.columnNames)
+
 
     def loopThroughLeadsAndDoStuff(self):
         collegeGreenLightDataInfo = CollegeGreenLightLeadsGetDatabaseInfo()
@@ -22,69 +20,9 @@ class PopulateCollegeGreenLightRequirements(object):
             descriptionSourceText = listOfConcatenatedDescriptionSourceText[i]
             descriptionSourceTextSentences = self.tokenizeIntoSentences(descriptionSourceText)
 
-            gpa = self.getGPAFromDescriptionSourceText(descriptionSourceTextSentences)
-            majors = self.getMajorFromDescriptionSourceText(descriptionSourceTextSentences)
+            gpa = self.getGPAFromSentences(descriptionSourceTextSentences)
+            majors = self.getMajorFromSentences(descriptionSourceTextSentences)
 
             self.doDatabaseInserts(collegeGreenLightLeadId, gpa, majors)
-
-    def tokenizeIntoSentences(self, stringToTokenize):
-        sentences = self.sentenceTokenizer.tokenize(stringToTokenize)
-        return sentences
-
-    def getGPAFromDescriptionSourceText(self, descriptionSourceTextSentences):
-        gpa = []
-
-        for sentence in descriptionSourceTextSentences:
-            if len(sentence) <= 1000:
-                maybeGPA = GPA(sentence).getGPA()
-                if maybeGPA != '':
-                    gpa.append(maybeGPA)
-
-        gpa = list(set(gpa))
-        gpa = ', '.join(gpa)
-
-        return gpa
-
-    def getMajorFromDescriptionSourceText(self, descriptionSourceTextSentences):
-        majors = []
-
-        majorsList = GetFastFindMajorsList.getDefaultList()
-        majorsList = [major.lower() for major in majorsList]
-        majorsList = list(set(majorsList))
-
-        majorsListRegex = '|'.join(majorsList)
-
-        for sentence in descriptionSourceTextSentences:
-            if len(sentence) <= 1000:
-                maybeMajor = Majors(sentence, majorsListRegex).getMajors()
-                if maybeMajor != '':
-                    majors.append(maybeMajor)
-
-        majors = list(set(majors))
-        majors = ', '.join(majors)
-
-        return majors
-
-    def doDatabaseInserts(self, collegeGreenLightLeadId, gpa, majors):
-        if gpa != '':
-            self.insertGPAIntoCollegeGreenLightRequirements(collegeGreenLightLeadId, gpa)
-
-        if majors != '':
-            self.insertMajorsIntoCollegeGreenLightLeadRequirements(collegeGreenLightLeadId, majors)
-
-    def insertGPAIntoCollegeGreenLightRequirements(self, collegeGreenLightLeadId, gpa):
-        attributeId = '1'
-        attributeValue = gpa
-
-        self.db.insertUpdateOrDelete(
-            "insert into dbo.CollegeGreenLightRequirements (CollegeGreenLightLeadId, AttributeId, AttributeValue) values ('" + collegeGreenLightLeadId + "', '" + attributeId + "', '" + attributeValue + "')")
-
-    def insertMajorsIntoCollegeGreenLightLeadRequirements(self, collegeGreenLightLeadId, majors):
-        attributeId = '417'
-        attributeValue = majors
-
-        self.db.insertUpdateOrDelete(
-            "insert into dbo.CollegeGreenLightRequirements (CollegeGreenLightLeadId, AttributeId, AttributeValue) values ('" + collegeGreenLightLeadId + "', '" + attributeId + "', '" + attributeValue + "')")
-
 
 PopulateCollegeGreenLightRequirements().loopThroughLeadsAndDoStuff()
