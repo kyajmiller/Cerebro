c5307d1cfe02b0a0469dfc1dc05ef4aaf0e03168
zjm203 <zjm203@email.arizona.edu>
2015-08-12 15:18:21 -0700

    process iefa leads

---
+++ Classes/ProcessIefaLeads.py
@@ -0,0 +1,13 @@
+from Classes.IefaLeads import IefaLeads
+from Classes.InsertIefaLeadArrayIntoIefaLeadsDB import InsertIefaLeadArrayIntoIefaLeadsDB
+
+
+class ProcessIefaLeads(object):
+    @staticmethod
+    def getIefaLeadsAndInsertIntoDB():
+        iefaLeadsArrays = IefaLeads().loopOverResultsPagesAndDoStuff()
+        for leadArray in iefaLeadsArrays:
+            InsertIefaLeadArrayIntoIefaLeadsDB(leadArray)
+
+
+ProcessIefaLeads.getIefaLeadsAndInsertIntoDB()
