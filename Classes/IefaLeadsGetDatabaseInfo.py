884c6028780f42864ae934c271e5c443ab53c0c2
zjm203 <zjm203@email.arizona.edu>
2015-08-14 10:04:44 -0700

    initial commit

---
+++ Classes/IefaLeadsGetDatabaseInfo.py
@@ -0,0 +1,80 @@
+from Classes.SUDBConnect import SUDBConnect
+
+
+class IefaLeadsGetDatabaseInfo(object):
+    def __init__(self, tag=None):
+        self.tag = tag
+        self.db = SUDBConnect()
+
+    def getTitleConcatenatedDescriptionOtherCriteriaList(self):
+        wholeList = []
+
+        titles = self.getTitles()
+        concatenatedDescriptionOtherCriterias = self.getConcatenatedDescriptionOtherCriteria()
+
+        for i in range(len(titles)):
+            title = titles[i]
+            concatenatedItem = concatenatedDescriptionOtherCriterias[i]
+
+            listOfItems = [title, concatenatedItem]
+
+            wholeList.append(listOfItems)
+
+        return wholeList
+
+    def getTitles(self):
+        titles = []
+
+        if self.tag:
+            rows = self.db.getRows("select Name from dbo.IefaLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                titles.append(row.Name)
+        else:
+            rows = self.db.getRows("select Name from dbo.IefaLeads")
+            for row in rows:
+                titles.append(row.Name)
+
+        return titles
+
+    def getConcatenatedDescriptionOtherCriteria(self):
+        listConcatenatedItems = []
+
+        descriptions = self.getDescriptions()
+        otherCriterias = self.getOtherCriterias()
+
+        for i in range(len(descriptions)):
+            description = descriptions[i]
+            otherCritieria = otherCriterias[i]
+
+            concatenatedItem = '%s %s' % (description, otherCritieria)
+            listConcatenatedItems.append(concatenatedItem)
+
+        return listConcatenatedItems
+
+    def getDescriptions(self):
+        descriptions = []
+
+        if self.tag:
+            rows = self.db.getRows("select Description from dbo.IefaLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                descriptions.append(row.Description)
+        else:
+            rows = self.db.getRows("select Description from dbo.IefaLeads")
+            for row in rows:
+                descriptions.append(row.Description)
+
+        return descriptions
+
+    def getOtherCriterias(self):
+        otherCriteria = []
+
+        if self.tag:
+            rows = self.db.getRows("select OtherCriteria from dbo.IefaLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                otherCriteria.append(row.OtherCriteria)
+        else:
+            rows = self.db.getRows("select OtherCriteria from dbo.IefaLeads")
+            for row in rows:
+                otherCriteria.append(row.OtherCriteria)
+
+        return otherCriteria
