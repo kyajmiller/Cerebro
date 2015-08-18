0e74de3ab512eed2d25aa861a5ff87a654b6632a
zjm203 <zjm203@email.arizona.edu>
2015-08-18 16:09:19 -0700

    get stuff on current page

--- Classes/Scholarships360Leads.py
+++ Classes/Scholarships360Leads.py
@@ -1,3 +1,4 @@
+import re
 from selenium import webdriver
 from Classes.CleanText import CleanText
 from Classes.RipPage import RipPage
@@ -18,6 +19,53 @@ class Scholarships360Leads(object):
         while nextPageExists:
             self.goToNextPage()
             nextPageExists = self.checkIfNextPage()
+
+    def getResultsOnCurrentPage(self):
+        titlesList = self.getTitlesList()
+        urlsList = self.getUrlsList()
+        deadlinesList = self.getDeadlinesList()
+        amountsList = self.getAmountsList()
+
+    def getTitlesList(self):
+        titlesList = []
+
+        titlesDivs = self.driver.find_elements_by_xpath("//h2/a")
+        for title in titlesDivs:
+            titlesList.append(title.get_attribute('textContent'))
+
+        titlesList = [CleanText.cleanALLtheText(title) for title in titlesList]
+        return titlesList
+
+    def getUrlsList(self):
+        urlsList = []
+
+        urlsDivs = self.driver.find_elements_by_xpath("//h2/a")
+        for url in urlsDivs:
+            urlsList.append(url.get_attribute('href'))
+
+        return urlsList
+
+    def getDeadlinesList(self):
+        deadlinesList = []
+
+        deadlinesDivs = self.driver.find_elements_by_xpath("//span[@class='due']")
+        for deadline in deadlinesDivs:
+            deadlinesList.append(deadline.get_attribute('textContent'))
+
+        deadlinesList = [re.sub('Due:', '', deadline) for deadline in deadlinesList]
+        deadlinesList = [CleanText.cleanALLtheText(deadline) for deadline in deadlinesList]
+        return deadlinesList
+
+    def getAmountsList(self):
+        amountsList = []
+
+        amountsDivs = self.driver.find_elements_by_xpath("//span[@class='amount']")
+        for amount in amountsDivs:
+            amountsList.append(amount.get_attribute('textContent'))
+
+        amountsList = [re.sub('Amount:', '', amount) for amount in amountsList]
+        amountsList = [CleanText.cleanALLtheText(amount) for amount in amountsList]
+        return amountsList
 
     def goToNextPage(self):
         if self.checkIfNextPage():
