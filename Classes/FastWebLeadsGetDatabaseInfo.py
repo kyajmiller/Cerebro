a8f2ff623853b21757f0d55b5372deb7de03264d
zjm203 <zjm203@email.arizona.edu>
2015-08-18 14:44:24 -0700

    get stuff from database

---
+++ Classes/FastWebLeadsGetDatabaseInfo.py
@@ -0,0 +1,92 @@
+from Classes.SUDBConnect import SUDBConnect
+
+
+class FastWebLeadsGetDatabaseInfo(object):
+    def __init__(self, tag=None):
+        self.tag = tag
+        self.db = SUDBConnect()
+
+    def getTitles(self):
+        titles = []
+
+        if self.tag:
+            rows = self.db.getRows("select * from dbo.FastWebLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                titles.append(row.Name)
+        else:
+            rows = self.db.getRows("select * from dbo.FastWebLeads")
+            for row in rows:
+                titles.append(row.Name)
+
+        return titles
+
+    def getDescriptions(self):
+        descriptions = []
+
+        if self.tag:
+            rows = self.db.getRows("select * from dbo.FastWebLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                descriptions.append(row.Description)
+        else:
+            rows = self.db.getRows("select * from dbo.FastWebLeads")
+            for row in rows:
+                descriptions.append(row.Description)
+
+        return descriptions
+
+    def getSourceTexts(self):
+        sourceTexts = []
+
+        if self.tag:
+            rows = self.db.getRows("select * from dbo.FastWebLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                sourceTexts.append(row.SourceText)
+        else:
+            rows = self.db.getRows("select * from dbo.FastWebLeads")
+            for row in rows:
+                sourceTexts.append(row.SourceText)
+
+        return sourceTexts
+
+    def getFastWebLeadsIds(self):
+        fastWebLeadsIds = []
+
+        if self.tag:
+            rows = self.db.getRows("select * from dbo.FastWebLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                fastWebLeadsIds.append(row.FastWebLeadId)
+        else:
+            rows = self.db.getRows("select * from dbo.FastWebLeads")
+            for row in rows:
+                fastWebLeadsIds.append(row.FastWebLeadId)
+
+        return fastWebLeadsIds
+
+    def getConcatenatedDescriptionSourceText(self):
+        concatenatedItems = []
+
+        descriptions = self.getDescriptions()
+        sourceTexts = self.getSourceTexts()
+
+        for i in range(len(descriptions)):
+            description = descriptions[i]
+            sourceText = sourceTexts[i]
+            concatenatedItem = '%s %s' % (description, sourceText)
+            concatenatedItems.append(concatenatedItem)
+
+        return concatenatedItems
+
+    def getTitleConcatenatedDescriptionSourceTextList(self):
+        wholeList = []
+
+        titles = self.getTitles()
+        concatenatedDescriptionSourceTexts = self.getConcatenatedDescriptionSourceText()
+
+        for i in range(len(titles)):
+            title = titles[i]
+            concatenatedItem = concatenatedDescriptionSourceTexts[i]
+
+            listOfItems = [title, concatenatedItem]
+            wholeList.append(listOfItems)
+
+        return wholeList
