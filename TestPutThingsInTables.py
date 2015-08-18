265251d6e4b918c0973a8d916ace0d20b9bcae1f
zjm203 <zjm203@email.arizona.edu>
2015-08-18 11:58:57 -0700

    unit test for update

--- TestPutThingsInTables.py
+++ TestPutThingsInTables.py
@@ -20,6 +20,27 @@ class TestStringMethods(unittest.TestCase):
         # tear down
         db.insertUpdateOrDelete("delete from dbo.Tests where Regex='testquery'")
 
+    def test_PutThingInTablesUpdate(self):
+        # setup
+        db = SUDBConnect()
+        createTestRow = PutThingsInTables('Tests', ['Regex', 'AttributeId'],
+                                          ['rowToUpdate', '94']).createSQLQueryInsert()
+        db.insertUpdateOrDelete(createTestRow)
+        self.assertIsNotNone("%s" % createTestRow)
+        rows = db.getRows("select * from dbo.Tests where Regex='rowToUpdate'")
+        testRegexValue = rows[0].Regex
+        self.assertEqual('rowToUpdate', testRegexValue)
+
+        # test
+        updateQuery = PutThingsInTables('Tests', ['Regex'], ['thisCellWasUpdated'], whereColumnNames=['AttributeId'],
+                                        whereValues=['94']).createSQLQueryUpdate()
+        db.insertUpdateOrDelete(updateQuery)
+        rows = db.getRows("select * from dbo.Tests where AttributeId='94'")
+        testUpdatedValue = rows[0].Regex
+        self.assertEqual('thisCellWasUpdated', testUpdatedValue)
+
+        # tear down
+        db.insertUpdateOrDelete("delete from dbo.Tests where AttributeId='94'")
 
 if __name__ == '__main__':
     unittest.main()
