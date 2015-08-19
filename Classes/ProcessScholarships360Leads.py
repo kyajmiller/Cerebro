326e39aac279501d4aeefef39473b1b86b68a545
zjm203 <zjm203@email.arizona.edu>
2015-08-19 14:31:55 -0700

    process the things

---
+++ Classes/ProcessScholarships360Leads.py
@@ -0,0 +1,14 @@
+from Classes.InsertScholarships360LeadArrayIntoScholarships360DB import \
+    InsertScholarships360LeadArrayIntoScholarships360DB
+from Classes.Scholarships360Leads import Scholarships360Leads
+
+
+class ProcessScholarships360Leads(object):
+    @staticmethod
+    def getScholarships360LeadsAndInsertIntoDatabase():
+        arrayOfScholarship360Leads = Scholarships360Leads().getLeads()
+        for leadArray in arrayOfScholarship360Leads:
+            InsertScholarships360LeadArrayIntoScholarships360DB(leadArray)
+
+
+ProcessScholarships360Leads.getScholarships360LeadsAndInsertIntoDatabase()
