7644e39861b3a09e57dfa29c8edab7bafd497e7f
zjm203 <zjm203@email.arizona.edu>
2015-08-24 13:38:47 -0700

    refactor

--- UnitTestNERTesting.py
+++ UnitTestNERTesting.py
@@ -7,19 +7,19 @@ class TestStringMethods(unittest.TestCase):
         nertest = NERTesting()
 
         sponsor1 = 'University of Arizona'
-        testSponsor1 = nertest.checkSponsorForBadInstitutions(sponsor1)
+        testSponsor1 = nertest.checkBadSponsor(sponsor1)
         self.assertFalse(testSponsor1)
 
         sponsor2 = 'University of Colorado'
-        testSponsor2 = nertest.checkSponsorForBadInstitutions(sponsor2)
+        testSponsor2 = nertest.checkBadSponsor(sponsor2)
         self.assertTrue(testSponsor2)
 
         sponsor3 = 'Chicken and Egg Group'
-        testSponsor3 = nertest.checkSponsorForBadInstitutions(sponsor3)
+        testSponsor3 = nertest.checkBadSponsor(sponsor3)
         self.assertFalse(testSponsor3)
 
         sponsor4 = 'School of Bacon'
-        testSponsor4 = nertest.checkSponsorForBadInstitutions(sponsor4)
+        testSponsor4 = nertest.checkBadSponsor(sponsor4)
         self.assertTrue(testSponsor4)
 
 
