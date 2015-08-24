e68f2795894126c31369111fc6e9b31f20d7cf9b
zjm203 <zjm203@email.arizona.edu>
2015-08-24 11:59:49 -0700

    unit test

---
+++ UnitTestNERTesting.py
@@ -0,0 +1,22 @@
+import unittest
+from Classes.NERTesting import NERTesting
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_checkSponsorBadInstitutions(self):
+        nertest = NERTesting()
+        sponsor1 = 'University of Arizona'
+        testSponsor1 = nertest.checkSponsorForBadInstitutions(sponsor1)
+        self.assertFalse(testSponsor1)
+
+        sponsor2 = 'University of Colorado'
+        testSponsor2 = nertest.checkSponsorForBadInstitutions(sponsor2)
+        self.assertTrue(testSponsor2)
+
+        sponsor3 = 'Chicken and Egg Group'
+        testSponsor3 = nertest.checkSponsorForBadInstitutions(sponsor3)
+        self.assertFalse(testSponsor3)
+
+
+if __name__ == '__main__':
+    unittest.main()
