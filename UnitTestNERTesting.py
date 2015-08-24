341757ca3b55c0475bc3152e79e67bed94dc0e3b
zjm203 <zjm203@email.arizona.edu>
2015-08-24 14:14:43 -0700

    unit tests for check bad text

--- UnitTestNERTesting.py
+++ UnitTestNERTesting.py
@@ -22,6 +22,18 @@ class TestStringMethods(unittest.TestCase):
         testSponsor4 = nertest.checkBadSponsor(sponsor4)
         self.assertTrue(testSponsor4)
 
+    def test_CheckBadText(self):
+        nertest = NERTesting()
+
+        infoText1 = 'I like cheese. Cheese tastes good.'
+        posTags = nertest.checkBadText(infoText1)
+
+        infoText2 = 'They refuse to permit us to obtain the refuse permit.'
+        posTags = nertest.checkBadText(infoText2)
+
+        infoText3 = "I don't know why they make cheese into an adjective."
+        posTags = nertest.checkBadText(infoText3)
+
 
 if __name__ == '__main__':
     unittest.main()
