0bb1f9ef84e129f370bf3d2d963c138c4b111905
zjm203 <zjm203@email.arizona.edu>
2015-08-28 14:45:02 -0700

    initial commit, no unit tests yet

---
+++ TestClassifyBadScholarshipNB.py
@@ -0,0 +1,10 @@
+import unittest
+from Classes.ClassifyBadScholarshipNB import ClassifyBadScholarshipNB
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_
+
+
+if __name__ == '__main__':
+    unittest.main()
