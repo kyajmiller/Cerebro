724d7c9e3f5774cb3b3307d05ee98a659de9f98d
zjm203 <zjm203@email.arizona.edu>
2015-10-02 10:35:38 -0700

    get database info, so far only has functions to get titles and descriptions

---
+++ Classes/MastersInEducationLeadsGetDatabaseInfo.py
@@ -0,0 +1,35 @@
+from Classes.SUDBConnect import SUDBConnect
+
+
+class MastersInEducationLeadsGetDatabaseInfo(object):
+    def __init__(self, tag=None):
+        self.tag = tag
+        self.db = SUDBConnect()
+
+    def getTitles(self):
+        titles = []
+
+        if self.tag:
+            rows = self.db.getRows("select * from dbo.MastersInEducationLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                titles.append(row.Name)
+        else:
+            rows = self.db.getRows("select * from dbo.MastersInEducationLeads")
+            for row in rows:
+                titles.append(row.Name)
+
+        return titles
+
+    def getDescriptions(self):
+        descriptions = []
+
+        if self.tag:
+            rows = self.db.getRows("select * from dbo.MastersInEducationLeads where Tag='" + self.tag + "'")
+            for row in rows:
+                descriptions.append(row.Description)
+        else:
+            rows = self.db.getRows("select * from dbo.MastersInEducationLeads")
+            for row in rows:
+                descriptions.append(row.Description)
+
+        return descriptions
