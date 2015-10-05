2f9edaa9ddc4ce67574c2232a789e3ff59aa009c
zjm203 <zjm203@email.arizona.edu>
2015-10-05 09:36:31 -0700

    get Scholarship Descriptions List function

--- ClassStandingClassifierStuff/GetDatabaseInfoScholarshipsWithClassStatuses.py
+++ ClassStandingClassifierStuff/GetDatabaseInfoScholarshipsWithClassStatuses.py
@@ -6,7 +6,18 @@ class GetDatabaseInfoScholarshipsWithClassStatuses(object):
         self.requirementNeeded = requirementNeeded
         self.formattedRequirementNeeded = '%' + self.requirementNeeded + '%'
         self.db = SUDBConnect()
-        print(self.formattedRequirementNeeded)
+
+        self.rows = self.db.getRows(
+            "select * from dbo.ScholarshipsWithClassStatuses where RequirementNeeded like '" + self.formattedRequirementNeeded + "'")
+
+    def getScholarshipDescriptionsList(self):
+        scholarshipDescriptionsList = []
+
+        for row in self.rows:
+            scholarshipDescriptionsList.append(row.ScholarshipDescription)
+
+        return scholarshipDescriptionsList
+
 
 
 GetDatabaseInfoScholarshipsWithClassStatuses("cheese")
