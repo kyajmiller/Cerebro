f98e0bbd4037d647b9c9e639d1f44f33f4eb914e
zjm203 <zjm203@email.arizona.edu>
2015-10-09 15:16:46 -0700

    initial commit, waiting to run

---
+++ ClassStandingClassifierStuff/TestClassifyClassStatus.py
@@ -0,0 +1,12 @@
+import unittest
+from ClassStandingClassifierStuff.ClassifyClassStatus import ClassifyClassStatus
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_classifyJuniorGetResults(self):
+        testClassify = ClassifyClassStatus(classStatus='Junior', trainingPercentage='0.8')
+        testClassify.trainTestAndGetResults()
+
+
+if __name__ == '__main__':
+    unittest.main()
