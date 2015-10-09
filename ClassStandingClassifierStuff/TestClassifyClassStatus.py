4e68740059b0cff094ee542cebd38d534e249a1e
zjm203 <zjm203@email.arizona.edu>
2015-10-09 15:50:20 -0700

    the percentage isn't supposed to be a string

--- ClassStandingClassifierStuff/TestClassifyClassStatus.py
+++ ClassStandingClassifierStuff/TestClassifyClassStatus.py
@@ -4,7 +4,7 @@ from ClassStandingClassifierStuff.ClassifyClassStatus import ClassifyClassStatus
 
 class TestStringMethods(unittest.TestCase):
     def test_classifyJuniorGetResults(self):
-        testClassify = ClassifyClassStatus(classStatus='Junior', trainingPercentage='0.8')
+        testClassify = ClassifyClassStatus(classStatus='Junior', trainingPercentage=0.8)
         testClassify.trainTestAndGetResults()
 
 
