06cdec6684175a3654025cd2852e6ad7412fbce2
zjm203 <zjm203@email.arizona.edu>
2015-10-12 15:51:29 -0700

    it was being dumb because I moved the unit tests

--- ClassStandingClassifierStuff/UnitTests/TestClassifyClassStatusFromPretrainedModel.py
+++ ClassStandingClassifierStuff/UnitTests/TestClassifyClassStatusFromPretrainedModel.py
@@ -6,16 +6,31 @@ from ClassStandingClassifierStuff.GetDatabaseInfoScholarshipsWithClassStatuses i
 
 class TestStringMethods(unittest.TestCase):
     def test_runModelOnFreshman(self):
-        trainedModel = 'ClassifierTrainedModels\FreshmanClassStatusTrainedLRModel'
-        trainedFeaturesValueCounts = 'ClassifierTrainedModels\FreshmanClassStatusTrainedFeaturesValueCounts'
+        classStatus = 'Freshman'
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % classStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % classStatus
         databaseInfo = GetDatabaseInfoScholarshipsWithClassStatuses('Freshman')
         dataTextList = databaseInfo.getConcatenatedDescriptionsEligibilities()
         idsList = databaseInfo.getScholarshipsWithClassStatusIdsList()
-        testClassify = ClassifyClassStatusFromPretrainedModel(trainedModelInputFile=trainedModel,
-                                                              trainedFeaturesValueCountsIndexesFile=trainedFeaturesValueCounts,
+        testClassify = ClassifyClassStatusFromPretrainedModel(trainedModelInputFile=modelSaveFile,
+                                                              trainedFeaturesValueCountsIndexesFile=featuresValueCountsSaveFile,
                                                               testingDataTextList=dataTextList,
                                                               testingDataIdsList=idsList)
         testClassify.displayResults()
+
+    def test_seeIfCanReadFile(self):
+        classStatus = 'Freshman'
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % classStatus
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % classStatus
+        inputFile1 = open(featuresValueCountsSaveFile, 'rb')
+        data = inputFile1.read()
+        print(len(data))
+        inputFile1.close()
+
+        inputFile2 = open(modelSaveFile, 'rb')
+        data = inputFile2.read()
+        print(len(data))
+        inputFile2.close()
 
 
 if __name__ == '__main__':
