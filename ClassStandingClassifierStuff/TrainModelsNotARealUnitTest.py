5ea79c8550e187727ff7564a918f839684168f44
zjm203 <zjm203@email.arizona.edu>
2015-10-12 13:28:57 -0700

    added test to run freshman train and save, not sure if the file path is right

--- ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
+++ ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
@@ -4,8 +4,9 @@ from ClassStandingClassifierStuff.ClassifyClassStatus import ClassifyClassStatus
 
 class TestStringMethods(unittest.TestCase):
     def test_trainFreshmanModel(self):
-        testClassify = ClassifyClassStatus(classStatus='Freshman', trainingPercentage=0.99)
-        testClassify.trainTestAndGetResults()
+        testClassify = ClassifyClassStatus(classStatus='Freshman', trainingPercentage=0.99,
+                                           modelSaveFile='ClassifierTrainedModels\FreshmanClassStatusTrainedLRModel')
+        testClassify.trainAndSaveModel()
 
 
 if __name__ == '__main__':
