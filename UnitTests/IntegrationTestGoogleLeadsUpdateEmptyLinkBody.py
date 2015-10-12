f5a29b568162a389ea3a860a628f8d45d22bf660
zjm203 <zjm203@email.arizona.edu>
2015-10-12 15:38:23 -0700

    put the unit tests in own folder, seeing all of them out in the open is driving me insane

---
+++ UnitTests/IntegrationTestGoogleLeadsUpdateEmptyLinkBody.py
@@ -0,0 +1,34 @@
+import unittest
+from Classes.GoogleLeadsUpdateEmptyLinkBody import GoogleLeadsUpdateEmptyLinkBody
+from Classes.InsertGoogleLeadArrayIntoGoogleLeadsDatabase import InsertGoogleLeadArrayIntoGoogleLeadsDatabase
+from Classes.SUDBConnect import SUDBConnect
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_GoogleLeadsUpdateEmptyLinkBody(self):
+        # set up
+        db = SUDBConnect()
+        db.insertUpdateOrDelete(
+            "delete from dbo.GoogleLeads where Link='http://colleges.fastweb.com/d-foreign-languages-literatures-and-linguistics'")
+
+        fakegoogleleadsarray = ['title', 'http://colleges.fastweb.com/d-foreign-languages-literatures-and-linguistics',
+                                'description']
+        InsertGoogleLeadArrayIntoGoogleLeadsDatabase(fakegoogleleadsarray)
+        rows1 = db.getRows(
+            "select * from dbo.GoogleLeads where Link='http://colleges.fastweb.com/d-foreign-languages-literatures-and-linguistics'")
+        self.assertEqual('title', rows1[0].Title)
+
+        # actual test of class
+        GoogleLeadsUpdateEmptyLinkBody()
+        rows2 = db.getRows(
+            "select * from dbo.GoogleLeads where Link='http://colleges.fastweb.com/d-foreign-languages-literatures-and-linguistics'")
+        self.assertEqual('title', rows2[0].Title)
+        self.assertGreater(len(rows2[0].LinkBody), 10)
+
+        # tear down
+        db.insertUpdateOrDelete(
+            "delete from dbo.GoogleLeads where Link='http://colleges.fastweb.com/d-foreign-languages-literatures-and-linguistics'")
+
+
+if __name__ == '__main__':
+    unittest.main()
