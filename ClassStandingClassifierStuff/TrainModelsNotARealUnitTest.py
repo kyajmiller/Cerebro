95cd15b7d05ce3778413d7db3a87662f38671c25
zjm203 <zjm203@email.arizona.edu>
2015-10-12 15:27:42 -0700

    even more better

--- ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
+++ ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
@@ -5,8 +5,17 @@ from ClassStandingClassifierStuff.ClassifyClassStatusUntrained import ClassifyCl
 class TestStringMethods(unittest.TestCase):
     def test_trainFreshmanModel(self):
         classStatus = 'Freshman'
-        modelSaveFile = 'ClassifierTrainedModels\FreshmanClassStatusTrainedLRModel'
-        featuresValueCountsSaveFile = 'ClasifierTrainedModels\FreshmanClassStatusTrainedFeaturesValueCounts'
+        modelSaveFile = 'ClassifierTrainedModels\%sClassStatusTrainedLRModel' % classStatus
+        featuresValueCountsSaveFile = 'ClasifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % classStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainSophomoreModel(self):
+        classStatus = 'Sophomore'
+        modelSaveFile = 'ClassifierTrainedModels\%sClassStatusTrainedLRModel' % classStatus
+        featuresValueCountsSaveFile = 'ClasifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % classStatus
         testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
