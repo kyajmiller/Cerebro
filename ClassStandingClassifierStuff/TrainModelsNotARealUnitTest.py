eb57087b2ef7aa3e6cdbef8a07a46b0588151c10
zjm203 <zjm203@email.arizona.edu>
2015-10-12 16:21:30 -0700

    added more things to train

--- ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
+++ ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
@@ -64,6 +64,76 @@ class TestStringMethods(unittest.TestCase):
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
 
+    def test_trainPhDLevelGraduateModel(self):
+        classStatus = 'Ph.D. Level Graduate'
+        fileClassStatus = 'PhDLevelGraduate'
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainNonDegreeSeekingGraduateModel(self):
+        classStatus = 'Non-degree Seeking Graduate'
+        fileClassStatus = 'NonDegreeSeekingGraduate'
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainMedicalSchoolStudentModel(self):
+        classStatus = 'Medical School Student'
+        fileClassStatus = re.sub(' ', '', classStatus)
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainPharmacySchoolStudentModel(self):
+        classStatus = 'Pharmacy School Student'
+        fileClassStatus = re.sub(' ', '', classStatus)
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainDNPModel(self):
+        classStatus = 'DNP'
+        fileClassStatus = re.sub(' ', '', classStatus)
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainPostBaccalaureateModel(self):
+        classStatus = 'Post-Baccalaureate'
+        fileClassStatus = 'PostBaccalaureate'
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
+    def test_trainPostdoctoralModel(self):
+        classStatus = 'Postdoctoral'
+        fileClassStatus = re.sub(' ', '', classStatus)
+        modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
+        featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
+        testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
+                                                     modelSaveFile=modelSaveFile,
+                                                     featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
+        testClassify.trainAndSaveModel()
+
     def test_seeIfCanWriteToFile(self):
         classStatus = 'Freshman'
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % classStatus
