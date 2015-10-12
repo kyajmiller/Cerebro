aeb398510ef34838677cfb665dee893ba1442144
zjm203 <zjm203@email.arizona.edu>
2015-10-12 16:27:03 -0700

    add print statements

--- ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
+++ ClassStandingClassifierStuff/TrainModelsNotARealUnitTest.py
@@ -6,6 +6,7 @@ from ClassStandingClassifierStuff.ClassifyClassStatusFromPreviouslyUntrainedMode
 class TestStringMethods(unittest.TestCase):
     def test_trainHighSchoolSeniorModel(self):
         classStatus = 'High School Senior'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -13,9 +14,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainFreshmanModel(self):
         classStatus = 'Freshman'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -23,9 +26,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainSophomoreModel(self):
         classStatus = 'Sophomore'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -33,9 +38,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainJuniorModel(self):
         classStatus = 'Junior'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -43,9 +50,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainSeniorModel(self):
         classStatus = 'Senior'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -53,9 +62,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainMastersLevelGraduateModel(self):
         classStatus = 'High School Senior'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -63,9 +74,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainPhDLevelGraduateModel(self):
         classStatus = 'Ph.D. Level Graduate'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = 'PhDLevelGraduate'
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -73,9 +86,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainNonDegreeSeekingGraduateModel(self):
         classStatus = 'Non-degree Seeking Graduate'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = 'NonDegreeSeekingGraduate'
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -83,9 +98,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainMedicalSchoolStudentModel(self):
         classStatus = 'Medical School Student'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -93,9 +110,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainPharmacySchoolStudentModel(self):
         classStatus = 'Pharmacy School Student'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -103,19 +122,23 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainDNPModel(self):
-        classStatus = 'DNP'
-        fileClassStatus = re.sub(' ', '', classStatus)
+        classStatus = 'Doctor of Nursing Practice (DNP)'
+        print("Training '%s' model..." % classStatus)
+        fileClassStatus = 'DNP'
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
         testClassify = ClassifyClassStatusTrainFirst(classStatus=classStatus, trainingPercentage=0.99,
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainPostBaccalaureateModel(self):
         classStatus = 'Post-Baccalaureate'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = 'PostBaccalaureate'
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -123,9 +146,11 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
     def test_trainPostdoctoralModel(self):
         classStatus = 'Postdoctoral'
+        print("Training '%s' model..." % classStatus)
         fileClassStatus = re.sub(' ', '', classStatus)
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % fileClassStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % fileClassStatus
@@ -133,6 +158,7 @@ class TestStringMethods(unittest.TestCase):
                                                      modelSaveFile=modelSaveFile,
                                                      featuresValuesCountsSaveFile=featuresValueCountsSaveFile)
         testClassify.trainAndSaveModel()
+        print("Training for '%s' complete." % classStatus)
 
 if __name__ == '__main__':
     unittest.main()
