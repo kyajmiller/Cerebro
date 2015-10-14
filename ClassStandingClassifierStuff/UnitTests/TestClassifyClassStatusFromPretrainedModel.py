5c7d4550567dbc360ec5497bfb66894a248c0d6b
zjm203 <zjm203@email.arizona.edu>
2015-10-14 14:05:30 -0700

    added check to make sure can access the files (make sure the relative file path is okay first)

--- ClassStandingClassifierStuff/UnitTests/TestClassifyClassStatusFromPretrainedModel.py
+++ ClassStandingClassifierStuff/UnitTests/TestClassifyClassStatusFromPretrainedModel.py
@@ -9,6 +9,14 @@ class TestStringMethods(unittest.TestCase):
         classStatus = 'Freshman'
         modelSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedLRModel' % classStatus
         featuresValueCountsSaveFile = '..\ClassifierTrainedModels\%sClassStatusTrainedFeaturesValueCounts' % classStatus
+
+        # test to make sure can open model and fvc save files:
+        testModelSaveOpen = open(modelSaveFile, 'rb')
+        testModelSaveOpen.close()
+        testFVCSaveOpen = open(featuresValueCountsSaveFile, 'rb')
+        testFVCSaveOpen.close()
+
+        # now do test
         databaseInfo = GetDatabaseInfoScholarshipsWithClassStatuses()
         dataTextList = databaseInfo.getConcatenatedDescriptionsEligibilities()
         idsList = databaseInfo.getScholarshipsWithClassStatusIdsList()
