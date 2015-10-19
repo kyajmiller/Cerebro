31a88fdff7e6fe447146554ffe00067bc01d1913
zjm203 <zjm203@email.arizona.edu>
2015-10-19 09:32:27 -0700

    new unit test, still unrun

--- ClassStandingClassifierStuff/UnitTests/TestEnsembleClassifyClassStatusFromPretrainedModel.py
+++ ClassStandingClassifierStuff/UnitTests/TestEnsembleClassifyClassStatusFromPretrainedModel.py
@@ -3,6 +3,7 @@ from ClassStandingClassifierStuff.EnsembleClassifyClassStatusFromPretrainedModel
     EnsembleClassifyClassStatusFromPretrainedModels
 from ClassStandingClassifierStuff.GetDatabaseInfoScholarshipsWithClassStatuses import \
     GetDatabaseInfoScholarshipsWithClassStatuses
+from Classes.SUDBConnect import SUDBConnect
 
 
 class TestStringMethods(unittest.TestCase):
@@ -18,6 +19,26 @@ class TestStringMethods(unittest.TestCase):
         # this is here to check if the relative file paths are right
         ensembleClassifyTest = EnsembleClassifyClassStatusFromPretrainedModels('blah', 'blah')
         ensembleClassifyTest.testToCheckRelativeFilePaths()
+
+    def test_RunEnsembleClassifierInsertResultsIntoDB(self):
+        # get data from db
+        databaseInfoExtract = GetDatabaseInfoScholarshipsWithClassStatuses()
+        dataTextList = databaseInfoExtract.getConcatenatedDescriptionsEligibilities()
+        idsList = databaseInfoExtract.getScholarshipsWithClassStatusIdsList()
+
+        # run classifier, return results
+        ensembleClassifyTest = EnsembleClassifyClassStatusFromPretrainedModels(dataTextList, idsList)
+        idsAndPredictionsList = ensembleClassifyTest.doAllClassificationsAndReturnFilteredPredictionsListsById()
+
+        # insert results into db
+        db = SUDBConnect()
+        ids = [prediction[0] for prediction in idsAndPredictionsList]
+        predictions = [prediction[1] for prediction in idsAndPredictionsList]
+        for scholarshipWithClassStatusID, prediction in zip(ids, predictions):
+            prediction = ', '.join(prediction)
+            db.insertUpdateOrDelete(
+                "update dbo.ScholarshipsWithClassStatuses set EnsembleClassifierPrediction='" + prediction + "' where ScholarshipsWithClassStatusId = '" + str(
+                    scholarshipWithClassStatusID) + "'")
 
 
 if __name__ == '__main__':
