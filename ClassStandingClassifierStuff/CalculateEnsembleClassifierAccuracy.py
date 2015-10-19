72fb4463f4dee3e82c968804330e915f83e5bb74
zjm203 <zjm203@email.arizona.edu>
2015-10-19 13:24:50 -0700

    calculate accuracy within labels lists, calculate percentage of labels where the number of predicted labels either matches, is greater than, or less than the number of actual labels

--- ClassStandingClassifierStuff/CalculateEnsembleClassifierAccuracy.py
+++ ClassStandingClassifierStuff/CalculateEnsembleClassifierAccuracy.py
@@ -47,5 +47,51 @@ class CalculateEnsembleClassifierAccuracy(object):
 
         print("Exact Match Accuracy: %.2f percent" % accuracy)
 
+    def calculateAverageAccuracyWithinLists(self):
+        total = len(self.actualLabels)
+        accuracyCount = 0
+
+        for actualLabel, predictedLabel in zip(self.actualLabels, self.predictedLabels):
+            numAccurateLabels = 0
+            totalLabels = len(predictedLabel)
+            if totalLabels > 0:
+                for label in predictedLabel:
+                    if label in actualLabel:
+                        numAccurateLabels += 1
+                labelAccuracy = numAccurateLabels / totalLabels
+                accuracyCount += labelAccuracy
+
+        averageAccuracy = (accuracyCount / total) * 100
+        print("Average Accuracy Across Labels: %.2f percent" % averageAccuracy)
+
+    def calculateNumLabelsAccuracy(self):
+        total = len(self.actualLabels)
+
+        numLabelsMatches = 0
+        numLabelsGreater = 0
+        numLabelsSmaller = 0
+
+        for actualLabel, predictedLabel in zip(self.actualLabels, self.predictedLabels):
+            if len(actualLabel) == len(predictedLabel):
+                numLabelsMatches += 1
+            elif len(predictedLabel) > len(actualLabel):
+                numLabelsGreater += 1
+            else:
+                numLabelsSmaller += 1
+
+        percentMatches = numLabelsMatches / total
+        percentGreater = numLabelsGreater / total
+        percentSmaller = numLabelsSmaller / total
+
+        print("Num labels matches: %.2f percent" % (percentMatches * 100))
+        print("Num labels greater: %.2f percent" % (percentGreater * 100))
+        print("Num labels smaller: %.2f percent" % (percentSmaller * 100))
+
+
+
+
+
 
 CalculateEnsembleClassifierAccuracy().calculateExactMatchAccuracy()
+CalculateEnsembleClassifierAccuracy().calculateAverageAccuracyWithinLists()
+CalculateEnsembleClassifierAccuracy().calculateNumLabelsAccuracy()
