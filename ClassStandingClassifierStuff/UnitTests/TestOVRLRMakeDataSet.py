b5cf7b83ffd33ac997c951f2ca3c4a96b30a5c6b
zjm203 <zjm203@email.arizona.edu>
2015-10-21 11:50:14 -0700

    rewrote unit tests to work with rewritten MakeDataSet class

--- ClassStandingClassifierStuff/UnitTests/TestOVRLRMakeDataSet.py
+++ ClassStandingClassifierStuff/UnitTests/TestOVRLRMakeDataSet.py
@@ -1,26 +1,50 @@
 import unittest
 import math
-from ClassStandingClassifierStuff.OVRLRMakeDataSetClassifyClassStatus import MakeDataSetClassifyClassStatus
+from ClassStandingClassifierStuff.MakeDataSetClassifyClassStatus import MakeDataSetClassifyClassStatus
+from ClassStandingClassifierStuff.GetDatabaseInfoScholarshipsWithClassStatuses import \
+    GetDatabaseInfoScholarshipsWithClassStatuses
 
 
 class TestStringMethods(unittest.TestCase):
     def test_makeTrainingTestingSets(self):
         desiredClassStatus = 'Junior'
-        setToWorkWith = MakeDataSetClassifyClassStatus(desiredClassStatus)
+        goodClassStatusDB = GetDatabaseInfoScholarshipsWithClassStatuses(requirementNeeded=desiredClassStatus)
+        badClassStatusDB = GetDatabaseInfoScholarshipsWithClassStatuses(requirementNeeded=desiredClassStatus,
+                                                                        useNot=True)
+
+        goodClassStatusIds = goodClassStatusDB.getScholarshipsWithClassStatusIdsList()
+        goodClassStatusDataTextList = goodClassStatusDB.getConcatenatedDescriptionsEligibilities()
+        badClassStatusIds = badClassStatusDB.getScholarshipsWithClassStatusIdsList()
+        badClassStatusDataTextList = badClassStatusDB.getConcatenatedDescriptionsEligibilities()
 
         trainingPercentage = 0.9
-        trainingSetAndTestingSet = setToWorkWith.makeTrainingAndTestingSets(trainingPercentage)
+        trainingSetAndTestingSet = MakeDataSetClassifyClassStatus.makeBinaryLabelTrainingAndTestingSet(
+            firstLabel=desiredClassStatus, secondLabel='Other', firstLabelTextList=goodClassStatusDataTextList,
+            secondLabelTextList=badClassStatusDataTextList, firstLabelIdsList=goodClassStatusIds,
+            secondLabelIdsList=badClassStatusIds, trainingPercentage=0.8)
         training = trainingSetAndTestingSet[0]
         testing = trainingSetAndTestingSet[1]
         totalSetSize = len(training) + len(testing)
         self.assertEqual(math.ceil(totalSetSize * trainingPercentage), len(training))
+        self.assertEqual((len(goodClassStatusDataTextList) + len(badClassStatusDataTextList)), totalSetSize)
 
     def test_makeTrainingTestingSetBadPercentage(self):
         desiredClassStatus = 'Senior'
-        setToWorkWith = MakeDataSetClassifyClassStatus(desiredClassStatus)
+        goodClassStatusDB = GetDatabaseInfoScholarshipsWithClassStatuses(requirementNeeded=desiredClassStatus)
+        badClassStatusDB = GetDatabaseInfoScholarshipsWithClassStatuses(requirementNeeded=desiredClassStatus,
+                                                                        useNot=True)
+
+        goodClassStatusIds = goodClassStatusDB.getScholarshipsWithClassStatusIdsList()
+        goodClassStatusDataTextList = goodClassStatusDB.getConcatenatedDescriptionsEligibilities()
+        badClassStatusIds = badClassStatusDB.getScholarshipsWithClassStatusIdsList()
+        badClassStatusDataTextList = badClassStatusDB.getConcatenatedDescriptionsEligibilities()
 
         trainingPercentage = 2
-        trainingSetAndTestingSet = setToWorkWith.makeTrainingAndTestingSets(trainingPercentage)
+        trainingSetAndTestingSet = MakeDataSetClassifyClassStatus.makeBinaryLabelTrainingAndTestingSet(
+            firstLabel=desiredClassStatus, secondLabel='Other', firstLabelTextList=goodClassStatusDataTextList,
+            secondLabelTextList=badClassStatusDataTextList, firstLabelIdsList=goodClassStatusIds,
+            secondLabelIdsList=badClassStatusIds, trainingPercentage=0.8)
+
         self.assertIsNone(trainingSetAndTestingSet)
 
 
