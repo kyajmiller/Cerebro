2cfe41e409db4a81474fa9e9960eaf64dfd999aa
zjm203 <zjm203@email.arizona.edu>
2015-10-21 13:30:53 -0700

    initial commit, goes to website, yay

---
+++ Classes/GoodCallLeads.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from Classes.CleanText import CleanText
+from Classes.RipPage import RipPage
+
+
+class GoodCallLeads(object):
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.base_url = "http://www.goodcall.com/"
+
+        self.driver.get(self.base_url + "scholarships/search")
+        self.driver.implicitly_wait(2)
+
+
+GoodCallLeads()
