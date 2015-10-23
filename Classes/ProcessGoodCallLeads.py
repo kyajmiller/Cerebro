13f951a2ffd1eb836edb61ef668191b5c7cabb2d
zjm203 <zjm203@email.arizona.edu>
2015-10-23 14:01:27 -0700

    initial commit, should run okay

---
+++ Classes/ProcessGoodCallLeads.py
@@ -0,0 +1,13 @@
+from Classes.GoodCallLeads import GoodCallLeads
+from Classes.InsertGoodCallLeadArrayIntoGoodCallLeadsDB import InsertGoodCallLeadArrayIntoGoodCallLeadsDB
+
+
+class ProcessGoodCallLeads(object):
+    @staticmethod
+    def getGoodCallLeadsAndInsertIntoDB():
+        goodCallLeadsArrays = GoodCallLeads().getLeads()
+        for leadArray in goodCallLeadsArrays:
+            InsertGoodCallLeadArrayIntoGoodCallLeadsDB(leadArray)
+
+
+ProcessGoodCallLeads.getGoodCallLeadsAndInsertIntoDB()
