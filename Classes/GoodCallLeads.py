23363ac7b512d01147f7cfc41082e20e695645f1
zjm203 <zjm203@email.arizona.edu>
2015-10-23 10:07:33 -0700

    get more things from result page

--- Classes/GoodCallLeads.py
+++ Classes/GoodCallLeads.py
@@ -42,6 +42,8 @@ class GoodCallLeads(object):
         geography = ''
         deadline = ''
         essayInfo = ''
+        essayPart1 = []
+        essayPart2 = []
         sourceWebsite = ''
         sourceText = ''
 
@@ -53,6 +55,45 @@ class GoodCallLeads(object):
         if self.checkIfElementExists("//div[@id='main-column']/p[2]"):
             sponsor = self.driver.find_element_by_xpath("//div[@id='main-column']/p[2]").get_attribute('textContent')
             sponsor = CleanText.cleanALLtheText(re.sub('Sponsor:', '', sponsor))
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'School Year')]/following-sibling::td"):
+            classStatus = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'School Year')]/following-sibling::td").get_attribute('textContent')
+            classStatus = CleanText.cleanALLtheText(classStatus)
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'School Year')]/following-sibling::td"):
+            major = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'School Year')]/following-sibling::td").get_attribute('textContent')
+            major = CleanText.cleanALLtheText(major)
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'Gender')]/following-sibling::td"):
+            gender = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'Gender')]/following-sibling::td").get_attribute('textContent')
+            gender = CleanText.cleanALLtheText(gender)
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'Ethnicity')]/following-sibling::td"):
+            ethnicity = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'Ethnicity')]/following-sibling::td").get_attribute('textContent')
+            ethnicity = CleanText.cleanALLtheText(ethnicity)
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'Grades')]/following-sibling::td"):
+            grades = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'Grades')]/following-sibling::td").get_attribute('textContent')
+            grades = CleanText.cleanALLtheText(grades)
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'Test Scores')]/following-sibling::td"):
+            testScores = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'Test Scores')]/following-sibling::td").get_attribute('textContent')
+            testScores = CleanText.cleanALLtheText(testScores)
+
+        if self.checkIfElementExists("//tr/td[contains(text(), 'Geography')]/following-sibling::td"):
+            geography = self.driver.find_element_by_xpath(
+                "//tr/td[contains(text(), 'Geography')]/following-sibling::td").get_attribute('textContent')
+            geography = CleanText.cleanALLtheText(geography)
+
+        if self.checkIfElementExists("//span[@class='deadline data']"):
+            deadline = self.driver.find_element_by_xpath("//span[@class='deadline data']").get_attribute('textContent')
+            deadline = CleanText.cleanALLtheText(re.sub('\(\.*?\)', '', deadline))
 
     def getTitlesList(self):
         titlesList = []
