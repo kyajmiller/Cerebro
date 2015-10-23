aac13b332e58635fc3125375fe509f5b43e1d07e
zjm203 <zjm203@email.arizona.edu>
2015-10-23 13:57:54 -0700

    added insert statement

--- Classes/InsertGoodCallLeadArrayIntoGoodCallLeadsDB.py
+++ Classes/InsertGoodCallLeadArrayIntoGoodCallLeadsDB.py
@@ -6,8 +6,8 @@ class InsertGoodCallLeadArrayIntoGoodCallLeadsDB(object):
         self.goodCallLeadArray = goodCallLeadArray
         self.db = SUDBConnect()
 
-        self.title = goodCallLeadArray[0]
-        self.resultPageLink = goodCallLeadArray[1]
+        self.name = goodCallLeadArray[0]
+        self.url = goodCallLeadArray[1]
         self.numAwards = goodCallLeadArray[2]
         self.amount = goodCallLeadArray[3]
         self.description = goodCallLeadArray[4]
@@ -23,3 +23,15 @@ class InsertGoodCallLeadArrayIntoGoodCallLeadsDB(object):
         self.essayInfo = goodCallLeadArray[14]
         self.sourceWebsite = goodCallLeadArray[15]
         self.sourceText = goodCallLeadArray[16]
+
+        if not self.checkIfAlreadyInDatabase():
+            self.db.insertUpdateOrDelete(
+                "insert into dbo.GoodCallLeads (Name, Url, NumAwards, Amount, Description, Sponsor, ClassStatus, Major, Gender, Ethnicity, Grades, TestScores, Deadline, EssayInfo, SourceWebsite, SourceText) values (N'" + self.name + "', N'" + self.url + "', N'" + self.numAwards + "', N'" + self.amount + "', N'" + self.description + "', N'" + self.sponsor + "', N'" + self.classStatus + "', N'" + self.major + "', N'" + self.gender + "', N'" + self.ethnicity + "', N'" + self.grades + "', N'" + self.testScores + "', N'" + self.deadline + "', N'" + self.essayInfo + "', N'" + self.sourceWebsite + "', N'" + self.sourceText + "')")
+
+    def checkIfAlreadyInDatabase(self):
+        matchingRow = self.db.getRows(
+            "select * from dbo.GoodCallLeads where Name='" + self.name + "' and Url='" + self.url + "'")
+        if matchingRow != []:
+            return True
+        else:
+            return False
