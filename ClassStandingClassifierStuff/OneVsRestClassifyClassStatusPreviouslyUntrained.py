5b5e5620afed90c08d70fb3aef51e0b799a17b93
zjm203 <zjm203@email.arizona.edu>
2015-10-30 10:47:07 -0700

    copied over makeFeaturesVectors function, finished trainOVRClassifier function

--- ClassStandingClassifierStuff/OneVsRestClassifyClassStatusPreviouslyUntrained.py
+++ ClassStandingClassifierStuff/OneVsRestClassifyClassStatusPreviouslyUntrained.py
@@ -27,7 +27,36 @@ class OneVsRestClassifyClassStatusPreviouslyUntrained(object):
         self.oneVsRestClassifier = OneVsRestClassifier(LogisticRegression())
 
     def trainOVRClassifier(self):
+        trainingFeaturesList = [trainingInstance['features'] for trainingInstance in self.trainingSet]
+        trainingLabelsList = [trainingInstance['label'] for trainingInstance in self.trainingSet]
 
+        featuresSeries = pandas.Series(list(itertools.chain(*trainingFeaturesList)))
+        featuresValueCounts = featuresSeries.value_counts()
+        self.featuresValueCountIndexes = featuresValueCounts.index
+
+        self.trainingVectors = self.makeFeaturesVectors(trainingFeaturesList, self.featuresValueCountIndexes)
+
+        self.oneVsRestClassifier.fit(self.trainingVectors, trainingLabelsList)
+
+    def makeFeaturesVectors(self, totalFeaturesList, featuresValueCountsIndexes):
+        featuresVectors = numpy.matrix(numpy.zeros((len(totalFeaturesList), featuresValueCountsIndexes.shape[0] + 1)))
+
+        # insert bias
+        featuresVectors[:, 0] = 1
+
+        for totalFeaturesIndex, totalFeaturesData in enumerate(totalFeaturesList):
+            # make regular vector
+            totalFeaturesData = pandas.Series(totalFeaturesData)
+            vectorCounts = totalFeaturesData.value_counts()
+
+            # make features vector
+            for featuresValueCountsIndexesIndex, featuresValueCountsIndexesValue in enumerate(
+                    featuresValueCountsIndexes):
+                if featuresValueCountsIndexesValue in vectorCounts.index:
+                    featuresVectors[totalFeaturesIndex, featuresValueCountsIndexesIndex + 1] = vectorCounts.ix[
+                        featuresValueCountsIndexesValue]
+
+        return featuresVectors
 
     def makeDataFrame(self):
         frame = pandas.DataFrame(columns=['label', 'scholarshipId', 'features'])
