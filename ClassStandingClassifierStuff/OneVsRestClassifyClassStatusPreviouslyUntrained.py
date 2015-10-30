c62c183a76f9f8dff56d23aeb3dd984fb1a9662a
zjm203 <zjm203@email.arizona.edu>
2015-10-30 10:06:44 -0700

    makes training/testing sets in the init, copied over self.MakeDataFrame function

--- ClassStandingClassifierStuff/OneVsRestClassifyClassStatusPreviouslyUntrained.py
+++ ClassStandingClassifierStuff/OneVsRestClassifyClassStatusPreviouslyUntrained.py
@@ -1,1 +1,26 @@
-__author__ = 'zjm203'
+import numpy
+import pandas
+import itertools
+import pickle
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
+from ClassStandingClassifierStuff.MakeDataSetClassifyClassStatus import MakeDataSetClassifyClassStatus
+
+
+class OneVsRestClassifyClassStatusPreviouslyUntrained(object):
+    def __init__(self, dataTextList, labelsList, idsList, trainingPercentage=0.8):
+        self.dataTextList = dataTextList
+        self.labelsList = labelsList
+        self.idsList = idsList
+        self.trainingPercentage = trainingPercentage
+
+        self.trainingSet, self.testingSet = MakeDataSetClassifyClassStatus.makeMultilabelTrainingAndTestingSet(
+            self.dataTextList, self.labelsList, self.idsList, self.trainingPercentage)
+        self.dataFrame = self.makeDataFrame()
+
+    def makeDataFrame(self):
+        frame = pandas.DataFrame(columns=['label', 'scholarshipId', 'features'])
+        for index, value in enumerate(self.testingSet):
+            frame.loc[index] = [value['label'], value['scholarshipId'], value['features']]
+
+        return frame
