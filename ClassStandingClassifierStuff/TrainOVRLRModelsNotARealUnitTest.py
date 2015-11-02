2ea6e2ce4c0b39de379eb4381f6f227537b68079
zjm203 <zjm203@email.arizona.edu>
2015-11-02 11:54:44 -0700

    created fake unit test to do training of classifier, untested

---
+++ ClassStandingClassifierStuff/TrainOVRLRModelsNotARealUnitTest.py
@@ -0,0 +1,31 @@
+import unittest
+import re
+from ClassStandingClassifierStuff.OneVsRestClassifyClassStatusPreviouslyUntrained import \
+    OneVsRestClassifyClassStatusPreviouslyUntrained
+import unittest
+from ClassStandingClassifierStuff.GetDatabaseInfoScholarshipsWithClassStatuses import \
+    GetDatabaseInfoScholarshipsWithClassStatuses
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_trainOVRLRModel(self):
+        modelSaveFile = 'OneVsRestLRTrainedClassifiers\OneVsRestLRTrainedModel'
+        featuresValueCountsSaveFile = 'OneVsRestLRTrainedClassifiers\OneVsRestLRTrainedFeaturesValueCounts'
+
+        # first check to see if can open files:
+        testOpenModelSaveFile = open(modelSaveFile, 'rb')
+        testOpenModelSaveFile.close()
+        testOpenFVCSaveFile = open(featuresValueCountsSaveFile, 'rb')
+        testOpenFVCSaveFile.close()
+
+        # do training and save to files
+        dataTextList = GetDatabaseInfoScholarshipsWithClassStatuses().getConcatenatedDescriptionsEligibilities()
+        labelsList = GetDatabaseInfoScholarshipsWithClassStatuses().getRequirementNeededList()
+        idsList = GetDatabaseInfoScholarshipsWithClassStatuses().getScholarshipsWithClassStatusIdsList()
+        testClassify = OneVsRestClassifyClassStatusPreviouslyUntrained(dataTextList, labelsList, idsList,
+                                                                       trainingPercentage=0.99)
+        testClassify.trainAndSaveOVRModel(modelSaveFile, featuresValueCountsSaveFile)
+
+
+if __name__ == '__main__':
+    unittest.main()
