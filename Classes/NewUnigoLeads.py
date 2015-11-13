f06915b75205e1f71b71586117959e19397d8f8a
zjm203 <zjm203@email.arizona.edu>
2015-11-13 11:16:44 -0700

    added function to log into unigo, needed a separate function because sometimes it takes you to different pages and is obnoxious

--- Classes/NewUnigoLeads.py
+++ Classes/NewUnigoLeads.py
@@ -10,5 +10,32 @@ class UnigoLeads(object):
         self.driver.get(self.base_url + "match/scholarshipresult#/login")
         self.driver.implicitly_wait(2)
 
+        self.driver.find_element_by_name('UserName').clear()
+        self.driver.find_element_by_name('UserName').send_keys('crawlyjones@gmail.com')
+        self.driver.find_element_by_name('password').clear()
+        self.driver.find_element_by_name('password').send_keys('sasgcoders626')
+        self.driver.find_element_by_css_selector('button.button.action').click()
+        self.driver.implicitly_wait(2)
+
+        self.logIn()
+
+    def logIn(self):
+        if self.driver.find_elements_by_name('UserName') and self.driver.find_elements_by_name('password'):
+            self.driver.find_element_by_name('UserName').clear()
+            self.driver.find_element_by_name('UserName').send_keys('crawlyjones@gmail.com')
+            self.driver.find_element_by_name('password').clear()
+            self.driver.find_element_by_name('password').send_keys('sasgcoders626')
+            self.driver.find_element_by_css_selector('button.button.action').click()
+            self.driver.implicitly_wait(2)
+        elif self.driver.find_elements_by_link_text('Log in'):
+            self.driver.find_element_by_link_text('Log in').click()
+            self.driver.implicitly_wait(2)
+            self.driver.find_element_by_name('UserName').clear()
+            self.driver.find_element_by_name('UserName').send_keys('crawlyjones@gmail.com')
+            self.driver.find_element_by_name('password').clear()
+            self.driver.find_element_by_name('password').send_keys('sasgcoders626')
+            self.driver.find_element_by_css_selector('button.button.action').click()
+            self.driver.implicitly_wait(2)
+
 
 UnigoLeads()
