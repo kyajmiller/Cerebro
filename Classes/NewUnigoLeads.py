08fee94e3a99b36dbe50bd9155060f4a53d6a03d
zjm203 <zjm203@email.arizona.edu>
2015-11-13 15:28:24 -0700

    I think it works now. I think so.

--- Classes/NewUnigoLeads.py
+++ Classes/NewUnigoLeads.py
@@ -18,6 +18,7 @@ class UnigoLeads(object):
     def getLeads(self):
         self.expandSeeMore()
         self.driver.implicitly_wait(2)
+        unigoLeadsArray = []
 
         arrayOfAmountObjects = self.driver.find_elements_by_xpath(
             "//div[@class='amount']/span[@data-bind='text: Aequitas.toCurrency(DollarAmount)']")
@@ -25,6 +26,84 @@ class UnigoLeads(object):
             "//h4[@data-bind='text: $parent.resultLayout ? shortTitle : Title']")
         arrayOfDeadlineObjects = self.driver.find_elements_by_xpath(
             "//h4[@data-bind='text: $parent.resultLayout ? shortTitle : Title']")
+
+        titlesList = self.getTitlesList(arrayOfTitleObjects)
+        amountsList = self.getAmountsList(arrayOfAmountObjects)
+        deadlinesList = self.getDeadlinesList(arrayOfDeadlineObjects)
+
+        for i in range(len(titlesList)):
+            title = CleanText.cleanALLtheText(titlesList[i])
+            amount = CleanText.cleanALLtheText(amountsList[i])
+            deadline = CleanText.cleanALLtheText(deadlinesList[i])
+
+            self.driver.get(self.base_url + 'match/scholarshipresult')
+            self.driver.implicitly_wait(2)
+
+            self.expandSeeMore()
+            arrayOfClickResultObjects = self.driver.find_elements_by_xpath(
+                "//a[@data-bind='click: function(scholarship, event) { $parent.showScholarshipDetail(scholarship, event) }']")
+            if arrayOfClickResultObjects[i]:
+                objectToClick = arrayOfClickResultObjects[i]
+
+                objectToClick.click()
+                self.driver.implicitly_wait(2)
+
+                resultPageArray = self.getResultPageInfo()
+                sponsor = resultPageArray[0]
+                awardAmount = resultPageArray[1]
+                recipients = resultPageArray[2]
+                requirements = resultPageArray[3]
+                additionalInfo = resultPageArray[4]
+                contact = resultPageArray[5]
+                address = resultPageArray[6]
+                sourceWebsite = resultPageArray[7]
+                sourceText = resultPageArray[8]
+
+                leadArray = [title, amount, deadline, sponsor, awardAmount, recipients, requirements, additionalInfo,
+                             contact, address, sourceWebsite, sourceText]
+                unigoLeadsArray.append(leadArray)
+
+        self.driver.quit()
+        return unigoLeadsArray
+
+    def getTitlesList(self, arrayOfTitleObjects):
+        titlesList = [titleObject.get_attribute('textContent') for titleObject in arrayOfTitleObjects]
+        return titlesList
+
+    def getAmountsList(self, arrayOfAmountObjects):
+        amountsList = [amountObject.get_attribute('textContent') for amountObject in arrayOfAmountObjects]
+        return amountsList
+
+    def getDeadlinesList(self, arrayOfDeadlineObjects):
+        deadlinesList = [deadlineObject.get_attribute('textContent') for deadlineObject in arrayOfDeadlineObjects]
+        return deadlinesList
+
+    def getResultPageInfo(self):
+        sponsor = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Awarded By']/../../following-sibling::div/p").get_attribute('textContent'))
+        awardAmount = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Award Amount']/../../following-sibling::div/p").get_attribute('textContent'))
+        recipients = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Recipients']/../../following-sibling::div/p").get_attribute('textContent'))
+        requirements = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Requirements']/../../following-sibling::div").get_attribute('textContent'))
+        additionalInfo = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Additional Information']/../../following-sibling::div/p").get_attribute(
+            'textContent'))
+        contact = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Contact']/../../following-sibling::div/p").get_attribute('textContent'))
+        address = CleanText.cleanALLtheText(self.driver.find_element_by_xpath(
+            "//div/p/strong[text() = 'Address']/../../following-sibling::div").get_attribute('textContent'))
+        if self.checkIfElementExists("//a[@class='button secondary']"):
+            sourceWebsite = self.driver.find_element_by_xpath("//a[@class='button secondary']").get_attribute('href')
+            sourceText = CleanText.cleanALLtheText(RipPage.getPageSource(sourceWebsite))
+        else:
+            sourceWebsite = ''
+            sourceText = ''
+
+        resultPageArray = [sponsor, awardAmount, recipients, requirements, additionalInfo, contact, address,
+                           sourceWebsite, sourceText]
+        return resultPageArray
 
     def logIn(self):
         if self.driver.find_elements_by_link_text('Log in'):
@@ -54,6 +133,13 @@ class UnigoLeads(object):
     def checkIfSeeMoreButtonExists(self):
         seeMoreButton = self.driver.find_elements_by_xpath("//a[@data-bind='click: showMoreScholarships']")
         if seeMoreButton != []:
+            return True
+        else:
+            return False
+
+    def checkIfElementExists(self, xpath):
+        checkElementExists = self.driver.find_elements_by_xpath(xpath)
+        if checkElementExists != []:
             return True
         else:
             return False
