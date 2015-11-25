3e543935a35d5a781b6a82286b0ecd8d54a3ce56
zjm203 <zjm203@email.arizona.edu>
2015-11-25 09:37:51 -0700

    test db insert update or delete function

--- UnitTests/TestSUDBConnect.py
+++ UnitTests/TestSUDBConnect.py
@@ -9,6 +9,20 @@ class TestStringMethods(unittest.TestCase):
         self.assertIsNotNone(rows)
         self.assertGreater(len(rows), 0)
 
+    def test_ConnectToDBInsertUpdateDelete(self):
+        db = SUDBConnect()
+        rows = db.getRows("select * from dbo.Tests")
+        self.assertGreater(len(rows), 0)
+        db.insertUpdateOrDelete("insert into dbo.Tests (Regex, AttributeId) values ('meow', 2)")
+        rows = db.getRows("select * from dbo.Tests where Regex='meow'")
+        self.assertGreater(len(rows), 0)
+        db.insertUpdateOrDelete("update dbo.Tests set Regex='kitty' where Regex='meow'")
+        rows = db.getRows("select * from dbo.Tests where Regex='kitty'")
+        self.assertGreater(len(rows), 0)
+        db.insertUpdateOrDelete("delete from dbo.Tests where Regex='kitty'")
+        rows = db.getRows("select * from dbo.Tests where Regex='kitty'")
+        self.assertEqual(len(rows), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
