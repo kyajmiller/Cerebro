0f57317c46ef96302645bcc2ab427c3598035ae0
zjm203 <zjm203@email.arizona.edu>
2015-12-07 16:11:59 -0700

    try this out, probably doesn't work but will see

--- Classes/WriteTablesToDisk.py
+++ Classes/WriteTablesToDisk.py
@@ -3,6 +3,19 @@ import re
 
 
 class WriteTablesToDisk(object):
-    def __init__(self, tableName):
+    def __init__(self, tableName, user):
         self.tableName = tableName
+        self.user = user
         self.website = re.sub('Leads', '', self.tableName)
+        self.sqlDB = SUDBConnect(destination='database')
+        self.fileDB = SUDBConnect(destination='filesystem')
+        self.columns = self.sqlDB.getColumnNamesFromTable(self.tableName)
+        self.rows = self.sqlDB.getRowsDB("select * from dbo.%s" % self.tableName)
+        self.urls = self.sqlDB.getRowsDB("select Url from dbo.%s" % self.tableName)
+
+        for row, url in zip(self.rows, self.urls):
+            values = row
+            self.fileDB.writeFile(self.columns, values, self.user, self.website, url)
+
+
+WriteTablesToDisk('CheggLeads', user='Kya')
