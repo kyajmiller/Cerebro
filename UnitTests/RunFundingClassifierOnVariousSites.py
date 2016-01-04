0bce1116777ac61a97bcbc907c58e07dcfb22f6b
zjm203 <zjm203@email.arizona.edu>
2016-01-04 10:13:44 -0700

    move rest of RunFundingClassifier classes to unit tests

--- UnitTests/RunFundingClassifierOnVariousSites.py
+++ UnitTests/RunFundingClassifierOnVariousSites.py
@@ -2,6 +2,10 @@ import unittest
 from Classes.RunFundingClassifier import RunFundingClassifier
 from Classes.CheggLeadsGetDatabaseInfo import CheggLeadsGetDatabaseInfo
 from Classes.FastWebLeadsGetDatabaseInfo import FastWebLeadsGetDatabaseInfo
+from Classes.FatomeiLeadsGetDatabaseInfo import FatomeiLeadsGetDatabaseInfo
+from Classes.IefaLeadsGetDatabaseInfo import IefaLeadsGetDatabaseInfo
+from Classes.MastersInEducationLeadsGetDatabaseInfo import MastersInEducationLeadsGetDatabaseInfo
+from Classes.Scholarships360GetDatabaseInfo import Scholarship360LeadsGetDatabaseInfo
 
 
 class TestStringMethods(unittest.TestCase):
@@ -23,6 +27,44 @@ class TestStringMethods(unittest.TestCase):
 
         fundingClassfier.getPredictedTagsInsertIntoDB(tableName, idColumnName, listFastWebLeadsIds)
 
+    def test_RunFundingClassifierOnFatomeiLeads(self):
+        titleDescriptionList = FatomeiLeadsGetDatabaseInfo().getTitleDescriptionList()
+        listFatomeiLeadsIds = FatomeiLeadsGetDatabaseInfo().getFatomeiLeadIds()
+        tableName = 'FatomeiLeads'
+        idColumnName = 'FatomeiLeadId'
+        fundingClassifier = RunFundingClassifier(titleDescriptionList)
+
+        fundingClassifier.getPredictedTagsInsertIntoDB(tableName, idColumnName, listFatomeiLeadsIds)
+
+    def test_RunFundingClassiferOnIefaLeads(self):
+        titleConcatenatedDescriptionOtherCriteriaList = IefaLeadsGetDatabaseInfo().getTitleConcatenatedDescriptionOtherCriteriaList()
+        listIefaLeadsIds = IefaLeadsGetDatabaseInfo().getIefaLeadsIds()
+        tableName = 'IefaLeads'
+        idColumnName = 'IefaLeadId'
+        fundingClassifier = RunFundingClassifier(titleConcatenatedDescriptionOtherCriteriaList)
+
+        fundingClassifier.getPredictedTagsInsertIntoDB(tableName, idColumnName, listIefaLeadsIds)
+
+    def test_RunFundingClassifierOnMastersInEducationLeads(self):
+        titleDescriptionList = MastersInEducationLeadsGetDatabaseInfo().getTitleDescriptionList()
+        listMIELeadsIds = MastersInEducationLeadsGetDatabaseInfo().getMastersInEducationLeadIds()
+        tableName = 'MastersInEducationLeads'
+        idColumnName = 'MastersInEducationLeadId'
+
+        fundingClassifier = RunFundingClassifier(titleDescriptionList)
+
+        fundingClassifier.getPredictedTagsInsertIntoDB(tableName, idColumnName, listMIELeadsIds)
+
+    def test_RunFundingClassifierOnScholarships360Leads(self):
+        scholarships360Info = Scholarship360LeadsGetDatabaseInfo()
+
+        titleDescriptionsList = scholarships360Info.getTitleDescriptionList()
+        listScholarships360LeadsIds = scholarships360Info.getScholarships360LeadsIds()
+        tableName = 'Scholarships360Leads'
+        idColumnName = 'Scholarship360LeadId'
+
+        fundingClassifier = RunFundingClassifier(titleDescriptionsList)
+        fundingClassifier.getPredictedTagsInsertIntoDB(tableName, idColumnName, listScholarships360LeadsIds)
 
 if __name__ == '__main__':
     unittest.main()
