5f1b8fe418edb635e5ce1a0590dfcfa0a63760e6
zjm203 <zjm203@email.arizona.edu>
2016-01-07 11:01:18 -0700

    add the functions, update the functions, yay

--- Classes/ProcessFastWebLeads.py
+++ Classes/ProcessFastWebLeads.py
@@ -1,13 +1,37 @@
 from Classes.InsertFastWebLeadsIntoFastWebLeadsDB import InsertFastWebLeadIntoFastWebLeadsDB
 from Classes.FastWebLeads import FastWebLeads
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
 
 
 class ProcessFastWebLeads(object):
     @staticmethod
     def getFastWebLeadsAndInsertIntoDB():
         fastWebLeadsArrays = FastWebLeads().getLeads()
-        for leadArray in fastWebLeadsArrays:
-            InsertFastWebLeadIntoFastWebLeadsDB(leadArray)
+        predictedFundingTypes = ProcessFastWebLeads.classifyFunding(fastWebLeadsArrays)
+        for leadArray, fundingClassification in zip(fastWebLeadsArrays, predictedFundingTypes):
+            badScholarshipClassification = ProcessFastWebLeads.checkBadScholarship(leadArray, fundingClassification)
+            InsertFastWebLeadIntoFastWebLeadsDB(leadArray, fundingClassification, badScholarshipClassification)
+
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = ['%s %s' % (leadArray[5], leadArray[9]) for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = leadArray[2]
+            infoText = '%s %s' % (leadArray[5], leadArray[9])
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
 
 
 ProcessFastWebLeads.getFastWebLeadsAndInsertIntoDB()
