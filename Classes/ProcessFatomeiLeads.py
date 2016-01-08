661e2ecd74945361d01879f60a08c62c09bc8e46
zjm203 <zjm203@email.arizona.edu>
2016-01-08 09:54:42 -0700

    add functions

--- Classes/ProcessFatomeiLeads.py
+++ Classes/ProcessFatomeiLeads.py
@@ -1,13 +1,36 @@
 from Classes.FatomeiLeads import FatomeiLeads
 from Classes.InsertFatomeiLeadsArrayIntoFatomeiLeadsDB import InsertFatomeiLeadsArrayIntoFatomeiLeadsDB
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
 
 
 class ProcessFatomeiLeads(object):
     @staticmethod
     def getFatomeiLeadsAndInsertIntoDB():
         fatomeiLeadsArrays = FatomeiLeads().getFatomeiLeadsArrays()
-        for leadArray in fatomeiLeadsArrays:
-            InsertFatomeiLeadsArrayIntoFatomeiLeadsDB(leadArray)
+        predictedFundingTypes = ProcessFatomeiLeads.classifyFunding(fatomeiLeadsArrays)
+        for leadArray, fundingClassification in zip(fatomeiLeadsArrays, predictedFundingTypes):
+            badScholarshipClassification = ProcessFatomeiLeads.checkBadScholarship(leadArray, fundingClassification)
+            InsertFatomeiLeadsArrayIntoFatomeiLeadsDB(leadArray, fundingClassification, badScholarshipClassification)
 
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = [leadArray[1] for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = ''
+            infoText = leadArray[1]
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
 
 ProcessFatomeiLeads.getFatomeiLeadsAndInsertIntoDB()
