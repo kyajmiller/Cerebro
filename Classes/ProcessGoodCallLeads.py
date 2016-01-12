7e7a1665e3532fc0eb2abc7c2c4067eba61c6051
zjm203 <zjm203@email.arizona.edu>
2016-01-12 09:45:46 -0700

    add functions to do bad scholarship and funding classification

--- Classes/ProcessGoodCallLeads.py
+++ Classes/ProcessGoodCallLeads.py
@@ -1,5 +1,7 @@
 from Classes.GoodCallLeads import GoodCallLeads
 from Classes.InsertGoodCallLeadArrayIntoGoodCallLeadsDB import InsertGoodCallLeadArrayIntoGoodCallLeadsDB
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
 
 
 class ProcessGoodCallLeads(object):
@@ -8,6 +10,26 @@ class ProcessGoodCallLeads(object):
         goodCallLeadsArrays = GoodCallLeads().getLeads()
         for leadArray in goodCallLeadsArrays:
             InsertGoodCallLeadArrayIntoGoodCallLeadsDB(leadArray)
+
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = [leadArray[4] for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = leadArray[5]
+            infoText = leadArray[4]
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
 
 
 ProcessGoodCallLeads.getGoodCallLeadsAndInsertIntoDB()
