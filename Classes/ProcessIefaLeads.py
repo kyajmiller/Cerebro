e3b123a8eeb27d75d2613457e5a5c3ea6f9d6849
zjm203 <zjm203@email.arizona.edu>
2016-01-12 14:24:12 -0700

    import the funding classification and bad scholarship functions

--- Classes/ProcessIefaLeads.py
+++ Classes/ProcessIefaLeads.py
@@ -1,5 +1,7 @@
 from Classes.IefaLeads import IefaLeads
 from Classes.InsertIefaLeadArrayIntoIefaLeadsDB import InsertIefaLeadArrayIntoIefaLeadsDB
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
 
 
 class ProcessIefaLeads(object):
@@ -9,4 +11,25 @@ class ProcessIefaLeads(object):
         for leadArray in iefaLeadsArrays:
             InsertIefaLeadArrayIntoIefaLeadsDB(leadArray)
 
-# ProcessIefaLeads.getIefaLeadsAndInsertIntoDB()
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = ['%s %s' % (leadArray[4], leadArray[5]) for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = leadArray[7]
+            infoText = '%s %s' % (leadArray[4], leadArray[5])
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
+
+
+ProcessIefaLeads.getIefaLeadsAndInsertIntoDB()
