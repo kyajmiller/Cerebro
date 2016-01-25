61c284f7b41ff48168ffb7f6eb43a9cd60932318
zjm203 <zjm203@email.arizona.edu>
2016-01-25 13:19:15 -0700

    add column for totalNewUpdated leads and total leads in table

--- Classes/CerebroLogs.py
+++ Classes/CerebroLogs.py
@@ -3,13 +3,20 @@ import time
 
 
 class CerebroLogs(object):
-    def __init__(self, website, totalEntries, numNewEntries, numUpdates):
+    def __init__(self, website, totalNewUpdated, numNewEntries, numUpdates):
         self.website = website
-        self.totalEntries = str(totalEntries)
+        self.totalEntries = str(totalNewUpdated)
         self.numNew = str(numNewEntries)
         self.numUpdates = str(numUpdates)
         self.date = time.strftime('%Y%m%d')
         self.db = SUDBConnect()
 
+        totalLeadsInTable = self.getTotalLeadsInTable()
+
         self.db.insertUpdateOrDeleteDB(
-            "insert into dbo.CerebroLogs (Website, Date, New, Updated, Total) values ('" + self.website + "', '" + self.date + "', '" + self.numNew + "', '" + self.numUpdates + "', '" + self.totalEntries + "')")
+                "insert into dbo.CerebroLogs (Website, Date, New, Updated, TotalNewUpdated, TotalLeads) values ('" + self.website + "', '" + self.date + "', '" + self.numNew + "', '" + self.numUpdates + "', '" + self.totalEntries + "', '" + totalLeadsInTable + "')")
+
+    def getTotalLeadsInTable(self):
+        tableName = '%sLeads' % self.website
+        rows = self.db.getRowsDB("select * from dbo.%s" % tableName)
+        return str(len(rows))
