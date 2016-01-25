f0b45aeec8568776d00ef2e939583868bf4bb6e3
zjm203 <zjm203@email.arizona.edu>
2016-01-25 10:57:52 -0700

    add insert to db statement, let's see what happens

--- Classes/CerebroLogs.py
+++ Classes/CerebroLogs.py
@@ -3,7 +3,13 @@ import time
 
 
 class CerebroLogs(object):
-    def __init__(self, website, entryType='New'):
+    def __init__(self, website, totalEntries, numNewEntries, numUpdates):
         self.website = website
-        self.entryType = entryType
+        self.totalEntries = totalEntries
+        self.numNew = numNewEntries
+        self.numUpdates = numUpdates
         self.date = time.strftime('%Y%m%d')
+        self.db = SUDBConnect()
+
+        self.db.insertUpdateOrDeleteDB(
+            "insert into dbo.CerebroLogs (Website, Date, New, Updated, Total) values ('" + self.website + "', '" + self.date + "', '" + self.numNew + "', '" + self.numUpdates + "', '" + self.totalEntries + "')")
