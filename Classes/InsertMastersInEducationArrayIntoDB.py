bca9ede1055511229497f92ea92f8a6026f99876
zjm203 <zjm203@email.arizona.edu>
2016-02-03 10:02:19 -0700

    needed to add an update statement, add variables for badscholarship, tag, and date, update sql to add those in

--- Classes/InsertMastersInEducationArrayIntoDB.py
+++ Classes/InsertMastersInEducationArrayIntoDB.py
@@ -1,10 +1,15 @@
 from Classes.SUDBConnect import SUDBConnect
+import re
+import time
 
 
 class InsertMastersInEducationArrayIntoDB(object):
-    def __init__(self, mastersInEducationLeadArray):
+    def __init__(self, mastersInEducationLeadArray, fundingClassification, badScholarshipClassification):
         self.mastersInEducationLeadArray = mastersInEducationLeadArray
+        self.fundingClassification = fundingClassification
+        self.badScholarshipClassificaion = badScholarshipClassification
         self.db = SUDBConnect()
+        self.fileSystemDB = SUDBConnect(destination='filesystem')
 
         self.name = mastersInEducationLeadArray[0]
         self.amount = mastersInEducationLeadArray[1]
@@ -12,15 +17,30 @@ class InsertMastersInEducationArrayIntoDB(object):
         self.description = mastersInEducationLeadArray[3]
         self.sourceWebsite = mastersInEducationLeadArray[4]
         self.sourceText = mastersInEducationLeadArray[5]
-
-        if not self.checkIfAlreadyInDatabase():
-            self.db.insertUpdateOrDeleteDB(
-                "INSERT INTO dbo.MastersInEducationLeads (Name, Deadline, Amount, Description, SourceWebsite, SourceText) VALUES (N'" + self.name + "', N'" + self.deadline + "', N'" + self.amount + "', N'" + self.description + "', N'" + self.sourceWebsite + "', N'" + self.sourceText + "')")
+        self.date = time.strftime('%Y%m%d')
 
     def checkIfAlreadyInDatabase(self):
         matchingRow = self.db.getRowsDB(
-            "select * from dbo.MastersInEducationLeads where Name='" + self.name + "' and Description='" + self.description + "'")
+                "select * from dbo.MastersInEducationLeads where Name='" + self.name + "' and SourceWebsite='" + self.sourceWebsite + "'")
         if matchingRow != []:
             return True
         else:
             return False
+
+    def writeFileToDisk(self):
+        tableName = 'MastersInEducationLeads'
+        user = 'Kya'
+        website = re.sub('Leads', '', tableName)
+        columns = self.db.getColumnNamesFromTable(tableName)
+        currentRow = self.db.getRowsDB(
+                "select * from dbo.MastersInEducationLeads where Name='" + self.name + "' and SourceWebsite='" + self.sourceWebsite + "'")[
+            0]
+        self.fileSystemDB.writeFile(columns, currentRow, user, website, self.sourceWebsite, self.date)
+
+    def insertUpdateLead(self):
+        if not self.checkIfAlreadyInDatabase():
+            self.db.insertUpdateOrDeleteDB(
+                    "INSERT INTO dbo.MastersInEducationLeads (Name, Deadline, Amount, Description, SourceWebsite, SourceText, Tag, BadScholarship, Date) VALUES (N'" + self.name + "', N'" + self.deadline + "', N'" + self.amount + "', N'" + self.description + "', N'" + self.sourceWebsite + "', N'" + self.sourceText + "', '" + self.fundingClassification + "', '" + self.badScholarshipClassificaion + "', '" + self.date + "')")
+        else:
+            self.db.insertUpdateOrDeleteDB(
+                "update dbo.MastersInEducationLeads set Deadline='" + self.deadline + "', Amount='" + self.amount + "', Description='" + self.description + "', SourceText='" + self.sourceText + "', Tag='" + self.fundingClassification + "', BadScholarship='" + self.badScholarshipClassificaion + "', Date='" + self.date + "'")
