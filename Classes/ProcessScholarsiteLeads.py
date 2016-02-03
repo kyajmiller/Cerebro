023e77cf11058f1249a080b37a7adf8a5e12b8ec
zjm203 <zjm203@email.arizona.edu>
2016-02-03 13:37:57 -0700

    add classify funding and checkbadscholarship

--- Classes/ProcessScholarsiteLeads.py
+++ Classes/ProcessScholarsiteLeads.py
@@ -1,5 +1,7 @@
 from Classes.ScholarsiteLeads import ScholarsiteLeads
 from Classes.InsertScholarsiteLeadsArrayIntoScholarsiteLeadsDB import InsertScholarsiteLeadsArrayIntoScholarsiteLeadsDB
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
 
 
 class ProcessScholarsiteLeads(object):
@@ -9,5 +11,24 @@ class ProcessScholarsiteLeads(object):
         for leadArray in scholarsiteLeadsArrays:
             InsertScholarsiteLeadsArrayIntoScholarsiteLeadsDB(leadArray)
 
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = [leadArray[3] for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = ''
+            infoText = leadArray[3]
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
 
 ProcessScholarsiteLeads.getScholarsiteLeadsAndInsertIntoDB()
