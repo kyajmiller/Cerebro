d16b1d74e0701306bf6251b4cead231b0fd35b8c
zjm203 <zjm203@email.arizona.edu>
2016-02-05 10:01:52 -0700

    add classify funding and check bad scholarship functions

--- Classes/ProcessUnigoLeads.py
+++ Classes/ProcessUnigoLeads.py
@@ -1,5 +1,7 @@
 from Classes.InsertUnigoLeadArrayIntoUnigoLeadsDB import InsertUnigoLeadArrayIntoUnigoLeadsDB
 from Classes.UnigoLeads import UnigoLeads
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
 
 
 class ProcessUnigoLeads(object):
@@ -8,6 +10,26 @@ class ProcessUnigoLeads(object):
         unigoLeadsArrays = UnigoLeads().getLeads()
         for leadArray in unigoLeadsArrays:
             InsertUnigoLeadArrayIntoUnigoLeadsDB(leadArray)
+
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = ['%s %s' % (leadArray[6], leadArray[7]) for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = leadArray[3]
+            infoText = '%s %s' % (leadArray[6], leadArray[7])
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
 
 
 ProcessUnigoLeads.getUnigoLeadsAndInsertIntoDB()
