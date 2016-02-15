8732f1e9f13bb0154acd8e09eeb0ca46e4642397
zjm203 <zjm203@email.arizona.edu>
2016-02-15 10:35:44 -0700

    initial commit, should go to the page

---
+++ Classes/TeacherDotOrgLeads.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from Classes.CleanText import CleanText
+from Classes.RipPage import RipPage
+
+
+class TeacherDotOrgLeads(object):
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.base_url = 'http://www.teacher.org/scholarships-grants/'
+        self.driver.get(self.base_url)
+        self.driver.implicitly_wait(2)
+
+
+TeacherDotOrgLeads()
