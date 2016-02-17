73051aacaafd7650e3af4d7d1ea09ce31ea190d9
zjm203 <zjm203@email.arizona.edu>
2016-02-17 10:18:48 -0700

    import classifyFuding and checkBadScholarship functions

--- Classes/ProcessTeacherDotOrgLeads.py
+++ Classes/ProcessTeacherDotOrgLeads.py
@@ -3,3 +3,29 @@ from Classes.TeacherDotOrgLeads import TeacherDotOrgLeads
 from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
 from Classes.ClassifyBadScholarships import ClassifyBadScholarships
 from Classes.CerebroLogs import CerebroLogs
+
+
+class ProcessTeacherDotOrgLeads(object):
+    @staticmethod
+    def getTeacherDotOrgLeadsAndInsertIntoDB():
+        teacherDotOrgLeadArrays = TeacherDotOrgLeads().getLeads()
+
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = ['%s %s' % (leadArray[1], leadArray[2]) for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = ''
+            infoText = '%s %s' % (leadArray[1], leadArray[2])
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
