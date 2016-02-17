7f9dd222b93575b563e027ed63de7ccacd903843
zjm203 <zjm203@email.arizona.edu>
2016-02-17 09:56:14 -0700

    initial commit

---
+++ Classes/InsertTeacherDotOrgLeadArrayIntoTeacherDotOrgDB.py
@@ -0,0 +1,8 @@
+from Classes.SUDBConnect import SUDBConnect
+import re
+import time
+
+
+class InsertTeacherDotOrgLeadArrayIntoTeacherDotOrgDB(object):
+    def __init__(self, teacherDotOrgLeadArray, fundingClassification, badScholarshipClassification):
+        pass
