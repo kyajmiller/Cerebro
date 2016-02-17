1d74a9213f2e37ce048e1c99ee8a5a3fe72a2f42
zjm203 <zjm203@email.arizona.edu>
2016-02-17 09:59:57 -0700

    add writeFileToDisk function

--- Classes/InsertTeacherDotOrgLeadArrayIntoTeacherDotOrgDB.py
+++ Classes/InsertTeacherDotOrgLeadArrayIntoTeacherDotOrgDB.py
@@ -17,3 +17,13 @@ class InsertTeacherDotOrgLeadArrayIntoTeacherDotOrgDB(object):
         self.sourceWebsite = teacherDotOrgLeadArray[3]
         self.sourceText = teacherDotOrgLeadArray[4]
         self.date = time.strftime('%Y%m%d')
+
+    def writeFileToDisk(self):
+        tableName = 'TeacherDotOrgLeads'
+        user = 'Kya'
+        website = re.sub('Leads', '', tableName)
+        columns = self.db.getColumnNamesFromTable(tableName)
+        currentRow = self.db.getRowsDB(
+                "select * from dbo.TeacherDotOrgLeads where Name='" + self.name + "' and SourceWebsite='" + self.sourceWebsite + "'")[
+            0]
+        self.fileSystemDB.writeFile(columns, currentRow, user, website, self.sourceWebsite, self.date)
