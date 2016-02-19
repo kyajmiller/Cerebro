0937ae15ec40bc4674dc3d4a873fc5087719cfb7
zjm203 <zjm203@email.arizona.edu>
2016-02-19 10:58:35 -0700

    initial commit

---
+++ Classes/ProcessTrafficSafetyStoreLeads.py
@@ -0,0 +1,12 @@
+from Classes.InsertTrafficSafetyStoreLeadsIntoTrafficSafetyStoreDB import \
+    InsertTrafficSafetyStoreLeadsIntoTrafficSafetyStoreDB
+from Classes.TrafficSafetyStoreLeads import TrafficSafetyStoreLeads
+from Classes.ClassifyFundingTypeKeywordBased import ClassifyFundingTypeKeywordBased
+from Classes.ClassifyBadScholarships import ClassifyBadScholarships
+from Classes.CerebroLogs import CerebroLogs
+
+
+class ProcessTrafficSafetyStoreLeads(object):
+    @staticmethod
+    def getTrafficSafetyStoreLeadsAndInsertIntoDB():
+        pass
