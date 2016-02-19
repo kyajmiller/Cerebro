64421e4e3ce98ea2d674db5bdc0623e8f6dbea6a
zjm203 <zjm203@email.arizona.edu>
2016-02-19 10:47:01 -0700

    unpack lead array

--- Classes/InsertTrafficSafetyStoreLeadsIntoTrafficSafetyStoreDB.py
+++ Classes/InsertTrafficSafetyStoreLeadsIntoTrafficSafetyStoreDB.py
@@ -10,3 +10,11 @@ class InsertTrafficSafetyStoreLeadsIntoTrafficSafetyStoreDB(object):
         self.badScholarshipClassification = badScholarshipClassification
         self.db = SUDBConnect()
         self.fileSystemDB = SUDBConnect(destination='filesystem')
+
+        self.name = trafficSafetyStoreLeadArray[0]
+        self.description = trafficSafetyStoreLeadArray[1]
+        self.eligibility = trafficSafetyStoreLeadArray[2]
+        self.award = trafficSafetyStoreLeadArray[3]
+        self.deadline = trafficSafetyStoreLeadArray[4]
+        self.sourceWebsite = trafficSafetyStoreLeadArray[5]
+        self.sourceText = trafficSafetyStoreLeadArray[6]
