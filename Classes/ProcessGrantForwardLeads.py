f2e458ac67e9b43f186eb90fa71c4ea00bab1f0f
zjm203 <zjm203@email.arizona.edu>
2016-02-24 13:25:24 -0700

    import classify funding, checkbadScholarship, begin getGrantForwardLeadsInsertIntoDB

--- Classes/ProcessGrantForwardLeads.py
+++ Classes/ProcessGrantForwardLeads.py
@@ -12,3 +12,29 @@ class ProcessGrantForwardLeads(object):
 
         for grantForwardLeadArray in self.arrayOfGrantForwardLeads:
             InsertGrantForwardLeadsArrayIntoGrantForwardItems(grantForwardLeadArray)
+
+    @staticmethod
+    def getGrantForwardLeadsInsertIntoDB():
+        majorsList = GetFastFindMajorsList.getGrantForwardItemsList()
+        for major in majorsList:
+            grantForwardLeadsArrays = GrantForwardLeads(major).processSearchResultsAndMakeLeadArray()
+
+    @staticmethod
+    def classifyFunding(leadsArrays):
+        titlesList = [leadArray[0] for leadArray in leadsArrays]
+        infoTextList = [leadArray[1] for leadArray in leadsArrays]
+        opportunitiesTitlesAndTexts = [[title, infoText] for title, infoText in zip(titlesList, infoTextList)]
+        fundingClassifier = ClassifyFundingTypeKeywordBased(opportunitiesTitlesAndTexts)
+        predictedFundingTypes = fundingClassifier.returnPredictedTags()
+        return predictedFundingTypes
+
+    @staticmethod
+    def checkBadScholarship(leadArray, fundingClassification):
+        if fundingClassification == 'Scholarship':
+            sponsor = ''
+            infoText = leadArray[1]
+            badScholarshipClassifier = ClassifyBadScholarships()
+            badScholarshipPrediction = badScholarshipClassifier.classifyOpportunity(sponsor, infoText)
+            return badScholarshipPrediction
+        else:
+            return ''
